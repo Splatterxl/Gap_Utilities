20134cb7309f8f87a3bd420d2cb0407976f0e25a
SplatterxlOfficial <67709748+Splatterxl@users.noreply.github.com>
2020-08-05 20:17:04 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -32,6 +32,17 @@ client.on("message", message => {
         client.commands.get('ping').execute(message, new Discord.MessageEmbed()
             .setColor('#0099ff').setTitle("🏓Ping!").addField("Online!", "OmniBot Is Online (Yay!)"));
     }
+    if (message.content.startsWith("o!config scan ")) {
+        if (message.content == "o!config scan true") {
+            config.scanEnabled = true;
+            message.channel.send("Property \`scan\` successfully updated to \`true\`.")
+            message.channel.send("All messages sent after this will be automatically scanned.")
+            message.channel.send("This feature is purely comic relief although it will become quite annoying.")
+        } else if (message.content = "o!config scan false") {
+            config.scanEnabled = false;
+            message.channel.send("Property \`scan\` successfully updated to \`false\`.")
+        }
+    }
 });
 
 class Config {
