4816125f4e44ab155de566111cc3541665d43c2c
SplatterxlOfficial <67709748+Splatterxl@users.noreply.github.com>
2020-08-09 19:16:30 +0100

    update Activities

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
+const activities_list = [
+    "to Splatterxl and Robotic Press", 
+    "to your feedback",
+    "to the o!help command", 
+    "to DiscordJS",
+    "to your commands"
+]; // creates an arraylist containing phrases you want your bot to switch through.
 
 client.on("ready", () => {
     console.log("Logged in as " + client.user.username + ".");
-    client.user.setActivity("your commands!", { "type": "LISTENING"})
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+    }, 10000); // Runs this every 10 seconds.
 });
 
 client.on("message", message => {
