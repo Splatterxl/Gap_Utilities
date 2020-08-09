690458b9954a199fe727f54d5f3c41a8dae0615a
SplatterxlOfficial <67709748+Splatterxl@users.noreply.github.com>
2020-08-09 19:17:46 +0100

    update bug

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.on("ready", () => {
     console.log("Logged in as " + client.user.username + ".");
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
     }, 10000); // Runs this every 10 seconds.
 });
 
