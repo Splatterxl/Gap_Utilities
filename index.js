31a3df0c77a09478612e36b58fad261cbedefe7e
SplatterxlOfficial <67709748+Splatterxl@users.noreply.github.com>
2020-08-10 21:29:09 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const activities_list = [
     "your feedback",
     "the o!help command", 
     "JavaScript",
-    "your commands"
+    "your commands",
+    "Premium Extravaganza Open! DM SplatterxlYT for info!"
 ]; // creates an arraylist containing phrases you want your bot to switch through.
 
 client.on("ready", () => {
     console.log("Logged in as " + client.user.username + ".");
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        let ActivityType = (index == 3) ? "PLAYING" : "LISTENING";
         client.user.setActivity(activities_list[index], {"type": "LISTENING"}); // sets bot's activities to one of the phrases in the arraylist.
     }, 10000); // Runs this every 10 seconds.
 });
