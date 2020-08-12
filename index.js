ba5313e22abf725fe4759716a7b4fc4bc8939efe
SplatterxlOfficial <67709748+Splatterxl@users.noreply.github.com>
2020-08-12 19:31:25 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 const activities_list = [
-    "Splatterxl and Robotic Press", 
+    "Splatterxl and Robotic Press",
     "your feedback",
-    "the o!help command", 
+    "the o!help command",
     "JavaScript",
     "your commands",
     "Premium Extravaganza Open! DM SplatterxlYT for info!",
@@ -16,18 +16,20 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.username}#${client.user.discriminator}.`);
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        client.user.setActivity(activities_list[index], {"type": "LISTENING"}); // sets bot's activities to one of the phrases in the arraylist.
+        client.user.setActivity(activities_list[index], {
+            "type": "LISTENING"
+        }); // sets bot's activities to one of the phrases in the arraylist.
     }, 10000); // Runs this every 10 seconds.
 });
 
 client.on("message", message => {
-    
+
     if (message.author.bot) return;
 
     console.log(`${message.author.username}#${message.author.discriminator} sent: "${message.content}" in text channel #${message.channel.name} in Guild ${message.guild.name}.\n`)
-    
+
     // ^^^ for local testing
-    
+
     if (config.settings.global.reactOnSend) {
         message.react("✅"); // reacts as soon as a message is sent.
     }
@@ -38,19 +40,19 @@ client.on("message", message => {
         message.channel.send(new Discord.MessageEmbed().setTitle("Scanning Message...").addField("Comic Relief Scan is running...", " ", false).setFooter("Why did you turn this on?"))
         message.channel.send(new Discord.MessageEmbed().setTitle("Scan Complete!").addField(`Message Approved:`, `\`"${message.content}"\``, false).setFooter("Is it annoying yet?"));
     }
-    
+
     if (message.content == "u!ping") {
         message.channel.send(new Discord.MessageEmbed().setColor('#0099ff').setTitle("🏓Ping!").addField("Online!", "OmniBot Is Online (Yay!)"));
-        
+
         // sends an embed back
-        
+
     } else if (message.content.startsWith("u!config scan ")) {
         if (message.content == "u!config scan true") {
             config.settings.global.scanEnabled = true;
             message.channel.send(new Discord.MessageEmbed().setTitle("Scan Enabled").addField("All messages sent after this will be automatically scanned.", "This feature is purely comic relief although it will become quite annoying."))
-            
+
             // confirms the change
-            
+
         } else if (message.content == "u!config scan false") {
             // Checking variables or strings uses == not =, = is used for declaration
             config.settings.global.scanEnabled = false;
@@ -59,13 +61,16 @@ client.on("message", message => {
     } else if (message.content == "u!support") {
         message.channel.send(new Discord.MessageEmbed().setTitle("You want Support?").addField("We got support!", "https://discord.gg/heD2x2K is the link!", false))
     } else if (message.content == "u!uwu") {
-        
+
         message.channel.send("**UwU**");
 
-    } else if (message.content == "u!help") {
-        
-        message.channel.send(new Discord.MessageEmbed().setTitle("Help").addField("User Commands", "`u!help user`", true).addField("Admin Commands", "`u!help admin`", true));
-        
+    } else if (message.content.startsWith("u!help")) {
+        if (message.content == "u!help") {
+            message.channel.send(new Discord.MessageEmbed().setTitle("Help").addField("User Commands", "`u!help user`", true).addField("Admin Commands", "`u!help admin`", true));
+        } else if (message.content == "u!help user") {
+            message.channel.send(new Discord.MessageEmbed().setTitle("User Commands").addField("Here are the commands that \@everyone can use.", "`u!help`: shows a Help Embed.\n`u!uwu`: It\'s obvious, isn\'t it?", false))
+        }
+
         // sends an embed back.
     } else if (message.content == "u!f") {
 
