fea25e16fa93f5dc82afe4f61e745e1dde4ec557
SplatterxlOfficial <67709748+Splatterxl@users.noreply.github.com>
2020-08-16 21:56:34 +0100

    yep ok

--- testingBuild.js
+++ testingBuild.js
@@ -1,10 +1,14 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client({"presence": {"status":"dnd"}});
+const bot = new Discord.Client({
+    "presence": {
+        "status": "dnd"
+    }
+});
 const config = require("./config.json");
-const event = new Events();
+// const event = new Events();
 
 const activities_list = [
-    "Splatterxl", 
+    "Splatterxl",
     "your feedback",
     "JavaScript",
     "your commands",
@@ -13,7 +17,12 @@ const activities_list = [
 ]; // creates an arraylist containing phrases the bot will switch through.
 
 bot.on("ready", () => {
-    event.ready();
+    console.log(`Logged in as ${bot.user.tag}.`);
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        let TYPE = (activities_list[index] === "NOTICE ME SENPAI!!!!") ? "CUSTOM_STATUS" : "LISTENING";
+        bot.user.setActivity(activities_list[index], {"type": TYPE}); // sets bot's activities to one of the phrases in the arraylist.
+    }, 10000); // Runs this every 10 seconds.
 });
 
 bot.on("message", message => {
@@ -25,11 +34,11 @@ bot.on("messageUpdate", (oldMessage, newMessage) => {
 });
 
 
-function Events () {
+/*function Events () {
     this.ready = require("./events/ready");
     this.message = require("./events/message");
     this.messageUpdate = require("./events/messageUpdate");
-}
+}*/
 
 
 
