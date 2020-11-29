701ef3abc25a429a2f49415e7f3217de045301ac
Ruairí / Splatterxl <splatterxl@outlook.ie>
2020-11-29 16:45:28 +0000

    changed it to work

--- events/commandLoader.js
+++ events/commandLoader.js
@@ -3,7 +3,7 @@ const { readdirSync } = require('fs');
 
 let cmds = new Discord.Collection();
 
-for (let file of readdirSync(`./commands`).filter(f => f.endsWith(`.js`)))
+for (let file of readdirSync(`./gap_utilities/commands`).filter(f => f.endsWith(`.js`)))
 {
     cmds.set(file.replace(/\.js/, ''), require(`../commands/${file}`));
 }
