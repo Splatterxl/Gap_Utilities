5cad21f8ecd57fb32bf2888f55857061f0724242
Splatterxl <splatterxl@outlook.ie>
2020-12-05 11:40:53 +0000

    *commands

--- commands/kill.js
+++ commands/kill.js
@@ -25,7 +25,7 @@ module.exports = {
         ];
         try
         {
-            if (msg.mentions.everyone || (args[1] == 'everyone')) return (await msg.channel.send('A massacre has occured.\nEveryone is dead.\nRest in peace. <:rip:784743972802527232>')).react('784743972802527232');
+            if (msg.mentions.everyone || (args[1] == 'everyone')) return (await msg.channel.send('A massacre has occured.\nEveryone is dead.\nRest in peace. <:rip:784743972802527232>'));
             if (msg.mentions.users.first().bot) msg.reply('Do you really want to !!{action}!! a bot? Ok.'.replace(/\!\!\{action\}\!\!/, 'kill'));
             if (msg.mentions.users.first() !== msg.author)
                 msg.channel.send(responses[Math.floor(Math.random() * responses.length)].replace(/\!\!\{author\}\!\!/, msg.author.tag).replace(/\!\!\{recipient\}\!\!/, msg.mentions.users.first().tag)); else msg.channel.send(['Uhhhhh... let\'s not do that, !!{author}!!.', 'I\'m not gonna do that, !!{author}!!...', 'uhhhhhhhh... no.'][Math.floor(Math.random() * Math.floor(Math.PI))].replace(/\!\!\{author\}\!\!/, '**' + msg.author.tag + '**'));
