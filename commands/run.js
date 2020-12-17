15a1a927e0f5a7d68b583a76d2996d26d0c5a7d5
Splatterxl <splatterxl@outlook.ie>
2020-12-17 19:13:26 +0000

    event(msg): add[dynamic] && feat(cmds): add[run]

---
+++ commands/run.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+let embeds = require('../assets/embeds');
+const proc = require('child_process');
+
+module.exports = {
+    help: {
+        "name": ">ping",
+        "id": "eval",
+        "aliases": [
+            "ping",
+            "pong"
+        ],
+        "desc": "Test if the bot is online!",
+        "example": ">ping"
+    },
+    /**
+     * @param {Discord.Client} bot
+     * @param {Discord.Message | Discord.PartialMessage} msg
+     * @param {string[]} args
+     */
+    run: async (bot, msg, args) =>
+    {
+        try
+        {
+            let now = Date.now();
+            if (args.slice(1).join(' ') == 'process') return msg.reply(require('./mock').run(bot, msg, ['mock', 'oh', 'no,', 'dont', 'crash', 'me']));
+            proc.execSync('cd tmp && mkdir ' + now);
+            fs.writeFileSync(`./tmp/${now}/index`, `try {(()=>console.log(eval('${args.slice(1).join(' ').replace(/\`/g, '\`').replace(/\\/g, '\\\\').replace(/\'/, '\'')}')))()}catch (e) {console.log(e)}`);
+            msg.reply(`\`\`\`js\n${proc.execSync(`cd tmp && cd ${now} && node index && rm -rf ${now}`).toString()}\`\`\``);
+        } catch (e)
+        {
+            throw e;
+        }
+    }
+};
