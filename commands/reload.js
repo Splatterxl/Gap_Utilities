4fe2c594e93dbc8fcde19ca8ef37ee76e006f611
Splatterxl <splatterxl@outlook.ie>
2020-12-20 20:40:28 +0000

    e

--- commands/reload.js
+++ commands/reload.js
@@ -1,19 +1,47 @@
 const Discord = require('discord.js');
+const embeds = require('../assets/embeds');
 
 module.exports = {
-    help: {
-        name: '>reload',
-        id: 'reload',
-        desc: 'Reload a(ll) command(s)'
-    },
-    /**
-     * 
-     * @param {Discord.Client} bot 
-     * @param {Discord.Message} msg 
-     * @param {string[]} args 
-     */
-    run: async (bot, msg, args) =>
+  help: {
+    name: '>reload',
+    id: 'reload',
+    desc: 'Reload a(ll) command(s)'
+  },
+  /**
+   * 
+   * @param {Discord.Client} bot 
+   * @param {Discord.Message} msg 
+   * @param {string[]} args 
+   */
+  run: async (bot, msg, args) =>
+  {
+    if (msg.author.id === '728342296696979526')
     {
-        (require('../events/commandLoader'));
-    }
+      if (!args)
+      {
+        (require('../events/commandLoader'))(1);
+        msg.reply(new Discord.MessageEmbed({
+          title: '<a:check:790313499225096213> Done!',
+          description: 'All commands have been reloaded!'
+        }));
+      }
+      switch (args[1].toLowerCase())
+      {
+        case 'all':
+          (require('../events/commandLoader'))(1);
+          msg.reply(new Discord.MessageEmbed({
+            title: '<a:check:790313499225096213> Done!',
+            description: 'All commands have been reloaded!'
+          }));
+          break;
+        default:
+          if (global.cmds.get(args[1]) && global.cmds.get(args[1]).run)
+            global.cmds.set(args[1], require(`./${args[1]}`));
+          msg.reply(new Discord.MessageEmbed({
+            title: '<a:check:790313499225096213> Done!',
+            description: `\`${args[1]}\` has been reloaded!`
+          }));
+      }
+    } else return msg.reply(embeds.notWhitelisted());
+  }
 };
