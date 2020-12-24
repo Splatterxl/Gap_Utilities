7568f8888e10cee0b1b8f16bfc8e2a0b109e6e57
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2020-12-24 12:06:21 +0000

    Update run.js

--- commands/run.js
+++ commands/run.js
@@ -27,7 +27,7 @@ module.exports = {
             let now = Date.now();
             if (args.slice(1).join(' ') == 'process') return msg.reply(require('./mock').run(bot, msg, ['mock', 'oh', 'no,', 'dont', 'crash', 'me'])).catch(r => null);
             proc.execSync('cd tmp && mkdir ' + now);
-            fs.writeFileSync(`./tmp/${now}/index`, `try {(()=>console.log(eval(\`${args.slice(1).join(' ').replace(/\`/g, '\`').replace(/\\/g, '\\\\').replace(/\'/, '\'')}\`)))()}catch (e) {console.log(e)}`);
+            fs.writeFileSync(`./tmp/${now}/index`, `try {(()=>console.log(eval(\`${args.slice(1).join(' ').replace(/\`/g, '\\`').replace(/\\/g, '\\\\').replace(/\'/, '\\'')}\`)))()}catch (e) {console.log(e)}`);
             msg.reply(`\`\`\`js\n${proc.execSync(`cd tmp && cd ${now} && node index ${((require('os')).EOL === '\r\n') ? '' : `&& rm -rf ${now}`}`).toString()}\`\`\``).catch(r => null);
         } catch (e)
         {
