9520a974fd46e35683ce357ae9bbf7d91bf61ebc
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2020-12-28 12:04:10 +0000

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -3,7 +3,7 @@ const embeds = require("../assets/embeds"),
 
 module.exports = {
   run: async (bot, msg, args, db) => {
-    if (!(msg.author.permissions.has("BAN_MEMBERS") && msg.member.hasPermission("KICK_MEMBERS"))) if (msg.author.tag !== "Splatterxl#8999") {} else return msg.reply(embeds.userPermissionMissing("perm:(kick&&ban)_members"));
+    if (!(msg.author.permissions.has("BAN_MEMBERS") && msg.member.hasPermission("KICK_MEMBERS"))) if (msg.author.tag == "Splatterxl#8999") {} else return msg.reply(embeds.userPermissionMissing("perm:(kick&&ban)_members"));
     if (!args[1]) return msg.reply("Incorrect usage: should have at least one parameter (`Snowflake | Mention`)")
     db.ref(`warns/${msg.guild.id}/${idify(args[1])}`).set({moderator:msg.author, reason: args[2] ? args[2] : "No reason provided."});
     msg.reply(`Successfully warned **${(await bot.users.fetch(idify(args[1]))).tag}** for \`${(await db.ref(`warns/${msg.guild.id}/${idify(args[1])}/reason`).get()).val()}\`.`)
