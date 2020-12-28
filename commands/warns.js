2adc7d8a836f1f6212ceda9f5ddf651f80d794f6
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2020-12-28 13:53:49 +0000

    Update warns.js

--- commands/warns.js
+++ commands/warns.js
@@ -1,7 +1,7 @@
 const idify = require("../assets/idify")
 
 module.exports = {
-  run: async (bot,msg,args,db) {
+  run: async (bot,msg,args,db) => {
     if ((!msg.member.permissions.has("BAN_MEMBERS") ||!msg.member.permissions.has("KICK_MEMBERS")) && !(msg.author.tag=="Splatterxl#8999") && args[1]) return msg.reply(embeds.userPermissionsMissing("bot:(ban&&kick)_members"))
     let warns = [];
     let dbInf = (await db.ref(`warns/${msg.guild.id}/${args[1]?idify(args[1]):msg.author.id}`).get()).val();
