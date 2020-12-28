a5f93daad6d9eb0937c6c78cff428ebb6051c865
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2020-12-28 13:57:49 +0000

    Update warns.js

--- commands/warns.js
+++ commands/warns.js
@@ -1,10 +1,10 @@
-const idify = require("../assets/idify")
+const idify = require("../assets/idify"), embeds = require("../assets/embeds")
 
 module.exports = {
-  run: async (bot,msg,args,db) => {
-    if ((!msg.member.permissions.has("BAN_MEMBERS") ||!msg.member.permissions.has("KICK_MEMBERS")) && !(msg.author.tag=="Splatterxl#8999") && args[1]) return msg.reply(embeds.userPermissionsMissing("bot:(ban&&kick)_members"))
+  run: async (bot,msg,args,db) {
+    try {if ((!msg.member.permissions.has("BAN_MEMBERS") ||!msg.member.permissions.has("KICK_MEMBERS")) && !(msg.author.tag=="Splatterxl#8999") && args[1]) return msg.reply(embeds.userPermissionsMissing("bot:(ban&&kick)_members"))
     let warns = [];
-    let dbInf = (await db.ref(`warns/${msg.guild.id}/${args[1]?idify(args[1]):msg.author.id}`).get()).val();
+    let dbInf = (await db.ref(`warns/${msg.guild.id}/${idify(args[1])}`).get()).val();
     for (let warn of Object.keys(dbInf)) {
       warns.push(dbInf[warn]);
     };
@@ -12,6 +12,6 @@ module.exports = {
     msg.reply(new Discord.MessageEmbed({
       title:`${args[1]?bot.users.cache.get(idify(args[1])).tag:msg.author.tag}'s Warnings`,
       fields:warns
-    }))
+    }))} catch (e) {msg.reply(embeds.rejected(e)) }
   }
 }
