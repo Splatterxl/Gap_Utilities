c8756b1bb767c2cef3799d78f2f1d88040f313c7
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2020-12-28 12:36:26 +0000

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -6,6 +6,7 @@ module.exports = {
     if (!(msg.member.permissions.has("BAN_MEMBERS") && msg.member.permissions.has("KICK_MEMBERS"))) if (msg.author.tag == "Splatterxl#8999") {} else return msg.reply(embeds.userPermissionMissing("perm:(kick&&ban)_members"));
     if (!args[1]) return msg.reply("Incorrect usage: should have at least one parameter (`Snowflake | Mention`)");
     let index = 0;
+    if (!(await db.ref(`warns/${msg.guild.id}`).get()).val()) db.ref(`warns/${msg.guild.id}`).set({`${idify(args[0])}`:[]});
     db.ref(`warns/${msg.guild.id}/${idify(args[1])}`).set((async()=>{let warns = (await db.ref(`warns/${msg.guild.id}/${idify(args[1])}`).get()).val(); warns = warns ? warns : [];index = warns.push({moderator:msg.author, reason: args[2] ? args.slice(2).join(" ") : "No reason provided."}); return warns;})());
     msg.reply(`Successfully warned **${(await bot.users.fetch(idify(args[1]))).tag}** for \`${(await db.ref(`warns/${msg.guild.id}/${idify(args[1])}`).get()).val()[index]}\`.`)
   }
