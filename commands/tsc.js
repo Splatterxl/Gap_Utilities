f7cd3e278dfc90afd361b8d87919633ecdcf06d1
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2020-12-30 13:36:03 +0000

    Update tsc.js

--- commands/tsc.js
+++ commands/tsc.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, msg, args, db) => {
   let m = await msg.reply("Writing folders...");
   proc.execSync(`cd tmp && cd tsc && mkdir ${now} && cd ${now} && mkdir src && mkdir dist`);
   m = await m.edit("Done writing folders!\nWriting `tsconfig.json`...")
-  fs.writeFileSync(path.join(__dirname, "..", "tmp", "tsc", now), `{"compilerOptions": {"alwaysStrict": true,"checkJs": true,"allowUnusedLabels": false,"allowUnreachableCode": false,"declaration": true,"declarationMap": true,"resolveJsonModule": true,"outDir": "dist/"},"include":["src/**/*"],"compileOnSave": false,"exclude":["dist/**"]}`);
+  fs.writeFileSync(path.join(__dirname, "..", "tmp", "tsc", now, "tsconfig.json"), `{"compilerOptions": {"alwaysStrict": true,"checkJs": true,"allowUnusedLabels": false,"allowUnreachableCode": false,"declaration": true,"declarationMap": true,"resolveJsonModule": true,"outDir": "dist/"},"include":["src/**/*"],"compileOnSave": false,"exclude":["dist/**"]}`);
   m = await m.edit("Done writing folders!\nDone writing `tsconfig.json`!\nWriting TypeScript file...");
   fs.writeFileSync(path.join(__dirname, "..", "tmp", "tsc", now, "index.ts"), `(()=>{try{console.log(${args.slice(1).replace(/\\/, "\\\\").replace(/\`/, "\\`")})}catch(e){console.log(e)}})()`);
   m = await m.edit("Done writing folders!\nDone writing `tsconfig.json`!\nDone writing TypeScript file!\nCompiling (this may take a while)...");
