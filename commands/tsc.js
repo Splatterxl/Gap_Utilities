91c2c5e10ef1c6f16e9720ca40195e7d9fb21da2
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2020-12-30 13:30:18 +0000

    Update tsc.js

--- commands/tsc.js
+++ commands/tsc.js
@@ -11,6 +11,6 @@ module.exports.run = async (bot, msg, args, db) => {
   m = await m.edit("Done writing folders!\nDone writing `tsconfig.json`!\nDone writing TypeScript file!\nCompiling (this may take a while)...");
   let output = proc.execSync(`cd tmp && cd tsc && cd ${now} && tsc`).toString();
   if (output) return m.edit("Done writing folders!\nDone writing `tsconfig.json`!\nDone writing TypeScript file!\nCompiling failed with the following error:\n```ts\n"+output+"\n```");
-  else {m = await m.edit("Done writing folders!\nDone writing `tsconfig.json`!\nDone writing TypeScript file!\nDone compiling!\nRunning code...");output = proc.execSync(`cd tmp && cd tsc && cd ${now} && cd dist && node index.js`).toString();
+  else {m = await m.edit("Done writing folders!\nDone writing `tsconfig.json`!\nDone writing TypeScript file!\nDone compiling!\nRunning code...");output = proc.execSync(`cd tmp && cd tsc && cd ${now} && cd dist && node index.js`).toString();}
   m = await m.edit("Done writing folders!\nDone writing `tsconfig.json`!\nDone writing TypeScript file!\nDone compiling!\nDone running code! Output: ```js\n"+output+"\n```");
 }
