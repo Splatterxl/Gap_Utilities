406b0bd20c8217bfdb3a329e5b94350f5b5107b2
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2020-12-30 13:58:30 +0000

    Update tsc.js

--- commands/tsc.js
+++ commands/tsc.js
@@ -10,8 +10,8 @@ module.exports.run = async (bot, msg, args, db) => {
   m = await m.edit("Done writing folders!\nDone writing `tsconfig.json`!\nWriting TypeScript file...");
   fs.writeFileSync(path.join(__dirname, "..", "tmp", "tsc", now, "src", "index.ts"), `(()=>{try{console.log(${args.slice(1).join(" ").replace(/\\/, "\\\\").replace(/\`/, "\\`")})}catch(e){console.log(e)}})()`);
   m = await m.edit("Done writing folders!\nDone writing `tsconfig.json`!\nDone writing TypeScript file!\nCompiling (this may take a while)...");
-  try{output = proc.execSync(`cd tmp && cd tsc && cd ${now} && tsc`).toString();}catch(e){output = e}
-  if (output) return m.edit("Done writing folders!\nDone writing `tsconfig.json`!\nDone writing TypeScript file!\nCompiling failed with the following error:\n```ts\n"+output+"\n```");
+  try{output = proc.execSync(`cd tmp && cd tsc && cd ${now} && tsc`).toString();}catch(e){output = "It seems as though an error occurred. Don't worry, our ESLint elves are on the case and will locate the errors soon."}
+  if (output) {return m.edit("Done writing folders!\nDone writing `tsconfig.json`!\nDone writing TypeScript file!\nCompiling failed with the following error:\n```ts\n"+output+"\n```");}
   else {m = await m.edit("Done writing folders!\nDone writing `tsconfig.json`!\nDone writing TypeScript file!\nDone compiling!\nRunning code...");output = proc.execSync(`cd tmp && cd tsc && cd ${now} && cd dist && node index.js`).toString();}
   m = await m.edit("Done writing folders!\nDone writing `tsconfig.json`!\nDone writing TypeScript file!\nDone compiling!\nDone running code! Output: ```js\n"+output+"\n```");
 }
