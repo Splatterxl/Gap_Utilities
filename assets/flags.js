ee2184b3eefafa58e5e67c0a815c017359b8fe2d
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2020-12-31 12:47:25 +0000

    Update flags.js

--- assets/flags.js
+++ assets/flags.js
@@ -1,5 +1,5 @@
 module.exports = str => {
-  let flags = str.match(/((--\w+)|(-\w)/);
+  let flags = str.match(/((--\w+)|(-\w))/g);
   flags.forEach((v,i,a)=>a[i]=v.slice(v.startsWith("--")?2:1));
   return flags;
 }
