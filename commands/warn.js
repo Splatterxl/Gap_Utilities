5ad040fb8faa15c145696b43b4a0900e23596d27
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2021-01-02 13:19:28 +0000

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -3,13 +3,15 @@ const embeds = require("../assets/embeds"),
   crs = require("crypto-random-string")
 
 module.exports = {
-  run: async (bot, msg, args, db) => {
-    if (!(msg.member.permissions.has("BAN_MEMBERS") && msg.member.permissions.has("KICK_MEMBERS"))) if (msg.author.tag == "Splatterxl#8999") {} else return msg.reply(embeds.userPermissionMissing("perm:(kick&&ban)_members"));
+  run: async (bot, msg, args, db, flags) => {
+    if (!(msg.member.permissions.has("BAN_MEMBERS") && msg.member.permissions.has("KICK_MEMBERS"))) if (!(require("../whitelist").includes(msg.author.id))) return msg.reply(embeds.userPermissionsMissing("perm:(kick&&ban)_members"));
     if (!args[1]) return msg.reply("Incorrect usage: should have at least one parameter (`Snowflake | Mention`)");
-if (idify(args[1])===msg.author.id) return msg.reply("Don't warn yourself, dumdum")
+    if (idify(args[1])===msg.author.id) return msg.reply("Don't warn yourself, dumdum")
     let str = crs({length:5});
+    
     if (!(await db.ref(`warns/${msg.guild.id}`).get()).val()) db.ref(`warns/${msg.guild.id}`).set({"e":"e"});
     db.ref(`warns/${msg.guild.id}/${idify(args[1])}/${str}`).set({moderator:msg.author, reason: args[2] ? args.slice(2).join(" ") : "No reason provided."});
+    if (flags.includes("ban") && (await msg.guild.members.fetch(idify(args[1]))).bannable) (await msg.guild.members.fetch(idify(args[1]))).ban({reason:`[ Warn by ${msg.author.tag} with flag '--ban' ] ${args[2]?args.slice(2).join(" "):"No reason specified."}`})
     msg.reply(`Successfully warned **${(await bot.users.fetch(idify(args[1]))).tag}** for \`${(await db.ref(`warns/${msg.guild.id}/${idify(args[1])}/${str}/reason`).get()).val()}\`.`)
   }
 }
