acce886d945ab6f4442cc849e1eeaa2e1f2b5c0f
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2021-01-02 15:15:47 +0000

    Update voat.js

--- commands/voat.js
+++ commands/voat.js
@@ -1,3 +1,3 @@
 const D = require("discord.js"), e = D.MessageEmbed
 
-module.exports.run = (b,m,a,d,f)=>m.channel.send(new e({title:"Support us on Void Bots",description:`Enjoying Eureka? Why not vote for us at <https://www.voidbots.net/bot/${c.user.id}/vote>? It helps us a lot!`}))
+module.exports.run = (c,m,a,d,f)=>m.channel.send(new e({title:"Support us on Void Bots",description:`Enjoying Eureka? Why not vote for us at <https://www.voidbots.net/bot/${c.user.id}/vote>? It helps us a lot!`}))
