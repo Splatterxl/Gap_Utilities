b525affb3d700fb99798853e7d88f527abf8e9dc
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2021-01-03 15:01:13 +0000

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -11,7 +11,7 @@ module.exports = {
     
     if (!(await db.ref(`warns/${msg.guild.id}`).get()).val()) db.ref(`warns/${msg.guild.id}`).set({"e":"e"});
     db.ref(`warns/${msg.guild.id}/${idify(args[1])}/${str}`).set({moderator:msg.author, reason: args[2] ? args.slice(2).join(" ") : "No reason provided."});
-    if (flags.includes("ban") && (await msg.guild.members.fetch(idify(args[1]))).bannable) (await msg.guild.members.fetch(idify(args[1]))).ban({reason:`[ Warn by ${msg.author.tag} with flag '--ban' ] ${args[2]?args.slice(2).join(" "):"No reason specified."}`})
+    if (flags?.includes("ban") && (await msg.guild.members.fetch(idify(args[1]))).bannable) (await msg.guild.members.fetch(idify(args[1]))).ban({reason:`[ Warn by ${msg.author.tag} with flag '--ban' ] ${args[2]?args.slice(2).join(" "):"No reason specified."}`})
     msg.reply(`Successfully warned **${(await bot.users.fetch(idify(args[1]))).tag}** for \`${(await db.ref(`warns/${msg.guild.id}/${idify(args[1])}/${str}/reason`).get()).val()}\`.`)
   }
 }
