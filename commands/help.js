705f572ef19fbbe110b0ad186a9035a73c577a19
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2021-01-03 16:31:16 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@
 const Discord = require('discord.js');
 const fs = require('fs'), path = require("path")
 
-const cmds = fs.readdirSync(path.join(__dirname)).map(v=> require(`./${v}`).help);
+const cmds = fs.readdirSync(path.join(__dirname)).map(v=> v=="help.js"?module.exports.help:require(`./${v}`).help);
 const categories = cmds.map(v=>[v.name, v.category]);
 const catL = {"moderation":[], "anime":[], "utility":[], "whitelisted":[]};
    categories.forEach(v=>v[1]?catL[v[1].toLowerCase()].push(v[0]):undefined);
