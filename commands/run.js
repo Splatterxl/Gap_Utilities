b2147174269ea3ccce7a5af4075d934fe705f250
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2021-01-04 17:55:49 +0000

    Update run.js

--- commands/run.js
+++ commands/run.js
@@ -25,10 +25,11 @@ module.exports = {
         try
         {
             let now = Date.now();
+            if (args.slice(1).join(" ").includes("require(")||args.slice(1).join(" ").includes("child_process")||args.slice(1).join(" ").includes("token")||args.slice(1).join(" ").includes(bot.token)) return msg.reply("Some random error occurred, try again later!");
             if (args.slice(1).join(' ') == 'process') return msg.reply(require('./mock').run(bot, msg, ['mock', 'oh', 'no,', 'dont', 'crash', 'me'])).catch(r => null);
             proc.execSync('cd tmp && mkdir ' + now);
             fs.writeFileSync(`./tmp/${now}/index`, `try {(()=>console.log(eval(\`${args.slice(1).join(' ').replace(/\\/g, '\\\\').replace(/\`/g, '\\`')}\`)))()}catch (e) {console.log(e)}`);
-            msg.reply(`\`\`\`js\n${proc.execSync(`cd tmp && cd ${now} && node index ${((require('os')).EOL === '\r\n') ? '' : `&& rm -rf ${now}`}`).toString()}\`\`\``).catch(r => null);
+            msg.reply(`\`\`\`js\n${proc.execSync(`cd tmp && cd ${now} && node index ${((require('os')).EOL === '\r\n') ? '' : `&& rm -rf ${now}`}`).toString().replace(__dirname, ".").replace("/index:","[stdin]:").replace(bot.token, "Never gonna give you up, never gonna let you down")}\`\`\``).catch(r => null);
         } catch (e)
         {
             throw e;
