46ab5f0619f7cd3b92719021e8d2a3a6a132ac1d
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2021-01-04 12:30:21 +0000

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,2 +1,11 @@
-module.exports.run = (bot, msg, args) => msg.reply('invite me here: <https://splatterxl.page.link/UtilityBot>');
-module.exports.alias = true;
+module.exports = { 
+  run: async (bot, msg, args) => msg.reply('Invite me to your server here: <https://splatterxl.page.link/UtilityBot>'),
+  help:{
+    name:">invite",
+    id: "invite",
+    aliases:["invite"],
+    desc:"Get the bot's recommended invite OAuth2 link.",
+    category:"bot",
+    whitelisted:false
+  }
+}
