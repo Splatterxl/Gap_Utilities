551bdf9a77bd8c0c5bcf86b5a945c1168e3131e8
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2021-01-04 12:32:37 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 
 const Discord = require('discord.js');
 const fs=require("fs"),path=require("path"), cmds = fs.readdirSync(path.join(__dirname)).map(v=>(v=="help.js")?module.exports.help:require(`./${v}`).help); const categories = cmds.map(v=>[v?.id, v?.category]);
-const catL = {"moderation":[], "fun":[], "utility":[], "owner":[], "bot":[]};
+const catL = {"moderation":[], "fun":[], "utility":[], "owner":[], "bot":[],"imagegen":[]};
    categories.forEach(v=>v[1]?catL[v[1].toLowerCase()][catL[v[1].toLowerCase()].length]=(v[0]):undefined); console.log(categories)
 module.exports = {
     help: {
@@ -77,24 +77,27 @@ let commands = () =>
    
     let arr = [{
         name: 'Moderation',
-        value: catL.moderation.length,
+        value: `${catL.moderation.length} Commands`,
         inline: true
     }, {
         name: 'Fun',
-        value: catL.fun.length,
+        value: `${catL.fun.length} Commands`,
         inline: true
     }, {
         name: 'Utility',
-        value: catL.utility.length,
+        value: `${catL.utility.length} Commands`,
         inline: true
     }, {
         name: 'Whitelisted',
-        value: catL.owner.length,
+        value: `${catL.owner.length} Commands`,
         inline: true
     }, {
         name: "Bot",
-        value: catL.bot.length,
+        value: `${catL.bot.length} Commands`,
         inline: true
+    }, {
+        name: "Image Generation",
+        value:`${catL.imagegen.length} Commands`
     }];
 
     return arr;
