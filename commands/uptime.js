a37a6e6ad9739b127e11186414648cc86eb53c1e
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2021-01-05 14:08:16 +0000

    Create uptime.js

---
+++ commands/uptime.js
@@ -0,0 +1,13 @@
+const moment = require("moment");
+
+module.exports = {
+  help: {
+    name:">uptime",
+    id:"uptime",
+    aliases:["uptime","wakey"],
+    whitelisted:false,
+    desc:"Gets how long ago the bot was started",
+    category:"bot"
+  },
+  run:async (bot,msg,args,db,flags) => {msg.reply((moment(Date.now()-global.timestamp)).from(0))};
+}
