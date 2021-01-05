3777bd87daf6d538ae719731e9a71ef9db6d5e78
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2021-01-05 14:47:27 +0000

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -4,10 +4,10 @@ module.exports = {
   help: {
     name:">uptime",
     id:"uptime",
-    aliases:["uptime","wakey"],
+    aliases:["wakey"],
     whitelisted:false,
     desc:"Gets how long ago the bot was started",
     category:"bot"
   },
-  run:async (bot,msg,args,db,flags) => {msg.reply((moment(global.timestamp)).from(moment()).replace(/ago/g, ""))}
+  run:async (bot,msg,args,db,flags) => {msg.reply(`The bot has been online for **${(moment(global.timestamp)).from(moment()).replace(/ago/g, ""))}**.`}
 }
