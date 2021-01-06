c9e7f6736f29ed27fb533f2f5e05dd91d8dcf9e3
nearlySplat <splatterxl@outlook.ie>
2021-01-06 16:05:09 +0000

    Create support.js

---
+++ commands/support.js
@@ -0,0 +1,11 @@
+module.exports = {
+  help: {
+    name:">support",
+    id: "support",
+    aliases: ["server","helpisontheway"],
+    desc:"Get the invite link of the Support Server",
+    example: "Support",
+    whitelisted: false
+  },
+  run: async(bot,msg)=>msg.reply((await bot.guilds.cache.find(g=>g.name=="Eureka! Backend").fetchInvites())[0])
+}
