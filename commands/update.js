2c4623253480edf5426e3d04a1d4826280134c29
Ruairí Ó hUanacháin <splatterxl@outlook.ie>
2021-01-06 13:57:34 +0000

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -7,5 +7,5 @@ module.exports = {
     aliases: ["gitpull", "up!", "pull", "restart", "reboot"],
     category: "owner"
   },
-  run: async (bot, msg, args, db, flags) => { delete require.cache; await msg.channel.send("Restarting bot...").then(m => db.ref("updatem").set(`${m.channel.id}-${m.id}`)); require("child_process").execSync("pm2 restart 0"); }
+  run: async (bot, msg, args, db, flags) => { delete require.cache; await msg.channel.send("Restarting bot...").then(m => db.ref("updatem").set(`${m.channel.id}-${m.id}`)); require("child_process").execSync("git pull && pm2 restart 0"); }
 };
