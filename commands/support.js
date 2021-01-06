dc8a7e6e661e31d0b8c72db7ae30ba2412bfe4b7
nearlySplat <splatterxl@outlook.ie>
2021-01-06 16:21:24 +0000

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -7,5 +7,5 @@ module.exports = {
     example: "Support",
     whitelisted: false
   },
-  run: async(bot,msg)=>msg.reply((await bot.guilds.cache.find(g=>g.name=="Eureka! Backend").fetchInvites()).first())
+  run: async(bot,msg)=>msg.reply("discord.gg/"+(await bot.guilds.cache.find(g=>g.name=="Eureka! Backend").fetchInvites()).first().code)
 }
