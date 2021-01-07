a03ae7b4452d432844375280f6205e9f565757bb
nearlySplat <splatterxl@outlook.ie>
2021-01-07 13:41:08 +0000

    Update tag.js

--- commands/tag.js
+++ commands/tag.js
@@ -1,6 +1,3 @@
-const tags = {
-  "vote":"Here's your daily reminder to vote at <https://voidbots.net/bot/!!{botid}!!/vote>!",
-  "support":"Do `{prefix}support` to find the invite link!"
-};
+const tags = require("../assets/tags.json")
 
- module.exports.run = async(bot,msg,args)=>{if (args[1]) msg.reply(tags[args[1]]?tags[args[1]].replace(/!!{botid}!!/g, bot.user.id):"No such tag!");}
+ module.exports.run = async(bot,msg,args)=>{if (args[1]) msg.reply(tags.global[args[1]]?tags.global[args[1]].replace(/!!{botid}!!/g, bot.user.id):"No such tag!");}
