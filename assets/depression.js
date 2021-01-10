9257b6eae174294ef252b63f5f44edc4e16b2647
nearlySplat <splatterxl@outlook.ie>
2021-01-10 12:15:14 +0000

    Create depression.js

---
+++ assets/depression.js
@@ -0,0 +1,7 @@
+module.exports = (m, msg) => {
+  await m.react("796095862874308678");
+  const collector = m.createReactionCollector((r, u) => u.id == msg.author.id);
+  collector.on("collect", r => {
+    if (r.emoji.id == "796095862874308678") m.delete();
+  });
+}
