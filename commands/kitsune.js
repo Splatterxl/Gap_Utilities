830c95af79861b650416d37841d7bc2425920ea2
nearlySplat <splatterxl@outlook.ie>
2021-01-11 11:19:39 +0000

    Update kitsune.js

--- commands/kitsune.js
+++ commands/kitsune.js
@@ -30,14 +30,14 @@ module.exports = {
         {
             fetch('https://purrbot.site/api/img/sfw/kitsune/img').then(res => res.json()).then(body =>
             {
-                require("../assets/depression")(await msg.channel.send(new Discord.MessageEmbed({
+                require("../assets/depression")((await msg.channel.send(new Discord.MessageEmbed({
                     color: 'BLACK',
                     footer: {
                         text: 'Powered by *Purr*'
                     },
                     image: { url: body.link },
                     title: 'Here\'s your kitsune!'
-                })), msg);
+                }))), msg);
             });
         } catch {
 
