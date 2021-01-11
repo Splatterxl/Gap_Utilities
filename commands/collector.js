2d3bddd4fa5c309440da94bd400ff5c485eb351c
Splatterxl <splatterxl@outlook.ie>
2021-01-11 17:43:02 +0000

    smth

--- commands/collector.js
+++
@@ -1,4 +0,0 @@
-module.exports.run = (bot, msg, args, db, flags) => {
-  const collector = msg.channel.createMessageCollector((m3) => m3.author.id==msg.author.id, { time:15000 });
-  collector.on("collect", m=>m.reply("yay u sent message"))
-}
