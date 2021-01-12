bc5422cde91e04474446a7d2a28e6f173ceb4ce8
nearlySplat <splatterxl@outlook.ie>
2021-01-12 21:18:42 +0000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -34,8 +34,9 @@ module.exports = {
     {
         // if ((!(msg.author.id === '728342296696979526')) || (!(require('../whitelist').includes(msg.author.id)))) return msg.channel.send("You are not in the whitelist or you do not have the `ADMINISTRATOR` permission.");
         if (!(whitelist.includes(msg.author.id))) return msg.channel.send(new error.HardcodedWhitelistError(`eval`, msg.author.id).result);
-        let raw = args.slice(2).join(' ');
+        
         let depth = parseInt(args[1]);
+        let raw = args.slice(isNaN(depth) ? 1 : 2).join(' ');
         if (isNaN(depth)) depth = 0;
         if (!raw) return msg.reply('You must specify code to execute.');
         let evalOutput;
