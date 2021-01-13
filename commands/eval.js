f8d4af395237a106ee32b688637af4f5e3fc024d
nearlySplat <splatterxl@outlook.ie>
2021-01-13 18:59:59 +0000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -32,28 +32,27 @@ module.exports = {
      */
     run: async (bot, msg, args, db, flags) =>
     {
-        // if ((!(msg.author.id === '728342296696979526')) || (!(require('../whitelist').includes(msg.author.id)))) return msg.channel.send("You are not in the whitelist or you do not have the `ADMINISTRATOR` permission.");
         if (!(whitelist.includes(msg.author.id))) return msg.channel.send(new error.HardcodedWhitelistError(`eval`, msg.author.id).result);
         
-        let depth = parseInt(args[1]);
-        let raw = args.slice(isNaN(depth) ? 1 : 2).join(' ');
+        let depth = parseInt(args[1]),
+          raw = args.slice(isNaN(depth) ? 1 : 2).join(' ');
         if (isNaN(depth)) depth = 0;
         if (!raw) return msg.reply('You must specify code to execute.');
         let evalOutput;
         try
         {
             evalOutput = (await eval(raw));
-        } catch (e) { evalOutput = e; }
+        } catch (e) { 
+            evalOutput = e; 
+        }
         const typ = typeof evalOutput;
         evalOutput = inspect(evalOutput, { depth: depth });
-        // try { evalOutput = eval(raw); } catch (e) { msg.reply(e); }
-        if (`${evalOutput}`.includes(bot.token)) evalOutput = `${evalOutput}`.replace(bot.token, '*'.repeat(bot.token.length));
-
-
         const evaled = evalOutput.match(/(\s|\S){1,1850}/g);
         let index = 0;
-        const em = await msg.channel.send(`\`\`\`js\n${evaled[index].replace(__dirname.replace(/((commands\/))/g, ""), "/root/eureka/")}\n\nTypeof output: ${typ}, Length: ${evalOutput.length}. Page ${index + 1} of ${evaled.length}\`\`\``).catch(e => msg.channel.send(embeds.rejected(e)));
-        ['❌', '⏮', "◀️", "▶️", '⏭', '🗑️'].map(v => em.react(v));
+        const em = await msg.channel.send(`Computing...`).catch(e => e);
+        function up() { em.edit(\`\`\`js\n${evaled[index]}\n\nTypeof output: ${typ}, Length: ${evalOutput.length}. Page ${index + 1} of ${evaled.length}\`\`\``) };
+        up()
+        ['❌', evaled.length >= 2'⏮', evaled.length >= 2 ? "◀️" : undefined, evaled.length >= 2 ? "▶️" : undefined, '⏭', '🗑️'].map(v => em.react(v).catch(e => e));
         const collector = em.createReactionCollector((r, u) => (u.id === msg.author.id));
         collector.on('collect', (r) =>
         {
@@ -64,32 +63,25 @@ module.exports = {
                     break;
                 case "▶️":
                     index = (index == (evaled.length - 1)) ? index : index + 1;
-                    em.edit(`\`\`\`js\n${evaled[index].replace(__dirname.replace(/((commands\/))/g, ""), "/root/eureka/")}\n\nTypeof output: ${typ}, Length: ${evalOutput.length}. Page ${index + 1} of ${evaled.length}\`\`\``);
+                    up()
                     break;
                 case "◀️":
                     index = index ? index - 1 : index;
-                    em.edit(`\`\`\`js\n${evaled[index].replace(__dirname.replace(/((commands\/))/g, ""), "/root/eureka/")}\n\nTypeof output: ${typ}, Length: ${evalOutput.length}. Page ${index + 1} of ${evaled.length}\`\`\``);
+                    up(); 
                     break;
                 case "⏮":
                     index = 0;
-                    em.edit(`\`\`\`js\n${evaled[index].replace(__dirname.replace(/((commands\/))/g, ""), "/root/eureka/")}\n\nTypeof output: ${typ}, Length: ${evalOutput.length}. Page ${index + 1} of ${evaled.length}\`\`\``);
+                    up();
                     break;
                 case "⏭":
                     index = evaled.length - 1;
-                    em.edit(`\`\`\`js\n${evaled[index].replace(__dirname.replace(/((commands\/))/g, ""), "/root/eureka/")}\n\nTypeof output: ${typ}, Length: ${evalOutput.length}. Page ${index + 1} of ${evaled.length}\`\`\``);
+                    up()
                     break;
                 case "🗑️":
                     em.delete();
                     collector.stop();
                     break;
             }
-        });
-
-        collector.on('end', c =>
-        {
-            console.log('Collected ${c.size} emojis');
-            if (msg.channel.messages.cache.get(em.id))
-                em.edit(em.content + `\nCollector closed. Collected ${c.size} reactions.`).catch(e => null);
-        });
+        });        
     }
 };
