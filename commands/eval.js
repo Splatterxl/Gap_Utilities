5acbab89ceabf08d02286bbf3d1b86556189c68e
nearlySplat <splatterxl@outlook.ie>
2021-01-13 21:03:54 +0000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -52,7 +52,7 @@ module.exports = {
         const em = await msg.channel.send(`Computing...`).catch(e => e);
         function up() { em.edit(`\`\`\`js\n${evaled[index]}\n\nTypeof output: ${typ}, Length: ${evalOutput.length}. Page ${index + 1} of ${evaled.length}\`\`\``) };
         up()
-        ['❌', '⏮', "◀️", "▶️", '⏭', '🗑️'].map(v => em.react(v).catch(e => e));
+        ['❌', '⏮', "◀️", "▶️", '⏭', '🗑️'].map(v => em.react(v));
         const collector = em.createReactionCollector((r, u) => (u.id === msg.author.id));
         collector.on('collect', (r) =>
         {
