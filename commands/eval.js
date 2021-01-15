a96b5e70f25aa2b8c8f54077290f91e34d9a9dc9
nearlySplat <splatterxl@outlook.ie>
2021-01-15 21:05:13 +0000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -8,6 +8,80 @@ const err = require('../misc/errorHandler');
 const Discord = require("discord.js");
 const { BaseCommand } = require('../structures/classes');
 
+module.exports = {
+  help: {
+
+            "name": ">eval",
+            "id": "eval",
+            "aliases": [
+                "eval",
+                "e"
+            ],
+            "desc": "A little evaluation command! (Restricted to owner.)",
+            "example": ">eval console.log(\"An Example.\")",
+            "category": "owner",
+            "whitelisted": true,
+            nsfw: false
+
+        },
+  run:  async (bot, msg, args, db, flags) =>
+        {
+            if (!(whitelist.includes(msg.author.id))) return msg.channel.send(new error.HardcodedWhitelistError(`eval`, msg.author.id).result);
+
+            let depth = parseInt(args[1]),
+                raw = args.slice(isNaN(depth) ? 1 : 2).join(' ');
+            if (isNaN(depth)) depth = 0;
+            if (!raw) return msg.reply('You must specify code to execute.');
+            let evalOutput;
+            try
+            {
+                evalOutput = (await eval(raw));
+            } catch (e)
+            {
+                evalOutput = e;
+            }
+            const typ = typeof evalOutput;
+            evalOutput = inspect(evalOutput, { depth: depth });
+            const evaled = evalOutput.match(/(\s|\S){1,1850}/g);
+            let index = 0;
+            const em = await msg.channel.send(`Computing...`).catch(e => e);
+            function up() { em.edit(`\`\`\`js\n${evaled[index]}\n\nTypeof output: ${typ}, Length: ${evalOutput.length}. Page ${index + 1} of ${evaled.length}\`\`\``); };
+            up();
+            [...'❌⏮◀️▶️⏭🗑️'].map(v => em.react(v));
+            const collector = em.createReactionCollector((r, u) => (u.id === msg.author.id));
+            collector.on('collect', (r) =>
+            {
+                switch (r.emoji.name)
+                {
+                    case '❌':
+                        em.edit('```\nEvaluation results closed.```');
+                        break;
+                    case "▶️":
+                        index = (index == (evaled.length - 1)) ? index : index + 1;
+                        up();
+                        break;
+                    case "◀️":
+                        index = index ? index - 1 : index;
+                        up();
+                        break;
+                    case "⏮":
+                        index = 0;
+                        up();
+                        break;
+                    case "⏭":
+                        index = evaled.length - 1;
+                        up();
+                        break;
+                    case "🗑️":
+                        em.delete();
+                        collector.stop();
+                        break;
+                }
+            });
+
+        };
+}
+/*
 module.exports = class Eval extends BaseCommand
 {
     constructor ()
@@ -35,7 +109,7 @@ module.exports = class Eval extends BaseCommand
             const em = await msg.channel.send(`Computing...`).catch(e => e);
             function up() { em.edit(`\`\`\`js\n${evaled[index]}\n\nTypeof output: ${typ}, Length: ${evalOutput.length}. Page ${index + 1} of ${evaled.length}\`\`\``); };
             up();
-            ['❌', '⏮', "◀️", "▶️", '⏭', '🗑️'].map(v => em.react(v));
+            [...'❌⏮◀️▶️⏭🗑️'].map(v => em.react(v));
             const collector = em.createReactionCollector((r, u) => (u.id === msg.author.id));
             collector.on('collect', (r) =>
             {
@@ -85,3 +159,4 @@ module.exports = class Eval extends BaseCommand
         }), cmd);
     }
 };
+*/
