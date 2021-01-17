b96bbf5f9c72289b28ce89d55750784b628d1171
nearlySplat <splatterxl@outlook.ie>
2021-01-17 13:25:24 +0000

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -11,6 +11,7 @@ module.exports = {
     help: {
         name: `>suggest`,
         id: `suggest`,
+        aliases: ["addsuggestion"]
         desc: `Suggest something about the bot.`,
         example: `>suggest test`
     },
