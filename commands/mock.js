79db18af3a9b5561f09582bff518ca7aacec3352
nearlySplat <splatterxl@outlook.ie>
2021-01-17 16:29:13 +0000

    Update mock.js

--- commands/mock.js
+++ commands/mock.js
@@ -4,4 +4,15 @@ const Discord = require('discord.js');
 module.exports.run = (bot, msg, args) => msg.reply(((str) => { let a = []; str.split("").forEach((v) => a.push(Math.round(Math.random()) == 1 ? v.toUpperCase() : v.toLowerCase(0))); return a.join(""); })(args.slice(1).join(' ')), {
 
 });
-module.exports.alias = true;
+module.exports.help = {
+        "name": ">mock",
+        "id": "mock",
+        "aliases": [
+            "mock",
+            "imitate"
+        ],
+        "desc": "Returns randomised letters.",
+        "example": ">mock hi",
+        "category": "fun",
+        "whitelisted": false
+    }
