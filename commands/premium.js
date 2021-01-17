05f09df3914b6c1c03a74346e3481c5b9cd519a4
nearlySplat <splatterxl@outlook.ie>
2021-01-17 17:14:43 +0000

    Update premium.js

--- commands/premium.js
+++ commands/premium.js
@@ -6,11 +6,13 @@ module.exports.run = async (bot, msg, args, db, flags) =>
         if (flags.getObj().solo.includes("c") || flags.getObj().solo.includes("check"))
             return msg.reply("Eureka! Premium is not enabled in your guild... big sad");
         else
-            if (flags.getObj().solo.includes("redeem") || flags.getObj().solo.includes("r"))
-                if ((await db.ref(`prem_codes/${args[2]}`).get()).val() && !(await db.ref(`premium/${msg.guild.id}`).get()).val())
+            if (flags.getObj().options.redeem || flags.getObj().options.r) {
+                const code = flags.getObj().options.r || flags.getObj().options.redeem
+                if ((await db.ref(`prem_codes/${r}`).get()).val() && !(await db.ref(`premium/${msg.guild.id}`).get()).val())
                     db.ref(`premium/${msg.guild.id}`).set(true).then(() => msg.reply("Done, this server will now enjoy Premium forever!"));
                 else
                     msg.reply("OwO whats this, an invalid code.");
+            }
             else
                 if (require('../whitelist').includes(msg.author.id) && flags.getObj().solo.includes('rm'))
                 {
@@ -18,3 +20,16 @@ module.exports.run = async (bot, msg, args, db, flags) =>
                 } else
                     return msg.reply('Unknown usage, proper usage should include 1+ flag(s), `-r | --redeem` or `-c | --check`');
 };
+
+module.exports.help = {
+        "name": ">premium",
+        "id": "premium",
+        "aliases": [
+            "premium",
+            "prem"
+        ],
+        "category": "bot",
+        "desc": "Use various different Premium utilities.",
+        "example": ">premium --check",
+        "whitelisted": false
+    }
