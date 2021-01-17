9bd477132080217e11cc452efa24eacbd369d803
nearlySplat <splatterxl@outlook.ie>
2021-01-17 17:39:14 +0000

    Update snipe.js

--- commands/snipe.js
+++ commands/snipe.js
@@ -23,6 +23,10 @@ module.exports = {
     run: async (bot, msg, args, db) =>
     {
         let snipe = global.snipes.get(msg.channel.id);
-        msg.reply(snipe ? new Discord.MessageEmbed({color:"YELLOW",description:`${snipe.editedTimestamp ? "Edit s" : "S"}nipe by ${snipe.author.tag} (${snipe.author.id})`, fields: [name:`${snipe.editedAt ? "New " : ""}Content`,value:snipe.content}) : 'None yet!');
+        msg.reply(snipe ? new Discord.MessageEmbed({
+          color: "YELLOW",
+          description: `${snipe.editedTimestamp ? "Edit s" : "S"}nipe by ${snipe.author.tag} (${snipe.author.id})`, 
+          fields: [{name:`${snipe.editedAt ? "New " : ""}Content`,value:snipe.content}]
+        }) : 'None yet!');
     }
 };
