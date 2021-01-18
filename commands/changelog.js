3466805734a221c5488eb8de5963bc951ce60571
nearlySplat <splatterxl@outlook.ie>
2021-01-18 17:48:39 +0000

    Create changelog.js

---
+++ commands/changelog.js
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const Discord = require('discord.js'),
+  pkg = require("../package.json")
+
+module.exports = {
+    help: {
+        "name": ">changelog",
+        "id": "changelog",
+        "aliases": [
+            
+        ],
+        "desc": "Get the changelog of a specific version of this bot.",
+        "example": ">changelog 3.3.0",
+        "category": "bot",
+        "whitelisted": false
+    },
+    /**
+     * @param {Discord.Client} bot
+     * @param {Discord.Message | Discord.PartialMessage} msg
+     * @param {string[]} args
+     */
+    run: async (bot, msg, args) =>
+    {
+        msg.channel.send(pkg.changelog[args[1].replace(/[v\^]/g, "") || pkg.version], { code: "md" })
+    }
+};
