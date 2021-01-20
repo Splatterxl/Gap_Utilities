accfae0813549a9b515d6bcf956841e27017be12
nearlySplat <splatterxl@outlook.ie>
2021-01-20 11:45:42 +0000

    Update tag.js

--- commands/tag.js
+++ commands/tag.js
@@ -1,3 +1,15 @@
 const tags = require("../misc/tags.json");
 
-module.exports.run = async (bot, msg, args) => { if (args[1]) msg.reply(tags.global[args[1]] ? tags.global[args[1]].replace(/!!{botid}!!/g, bot.user.id) : "No such tag!"); };
+module.exports.run = async (bot, msg, args, db, flags, ctx) => { if (args[1]) ctx.respond(tags.global[args[1]] ? tags.global[args[1]].replace(/!!{botid}!!/g, bot.user.id) : "No such tag!"); };
+
+module.exports.help = {
+        "name": ">tag",
+        "id": "tag",
+        "aliases": [
+            
+        ],
+        "desc": "get a tag from our json file its ez af",
+        "example": ">tag list",
+        "category": "utility",
+        "whitelisted": false
+    }
