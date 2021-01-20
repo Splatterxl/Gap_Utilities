67ef21acf75b4a9d01a78cfdc819d8c6794a8d83
nearlySplat <splatterxl@outlook.ie>
2021-01-20 20:38:41 +0000

    Create paginate.js

---
+++ misc/paginate.js
@@ -0,0 +1,36 @@
+module.exports = (m, pages, ctx) => {
+            let index = 0;
+            
+            function up() { m.edit(pages[index]); };
+            up();
+            ["⏮","◀️","▶️","⏭",'🗑️'].map(v => em.react(v));
+            const collector = em.createReactionCollector((r, u) => (u.id === ctx.message.author.id));
+            collector.on('collect', (r) =>
+            {
+                switch (r.emoji.name)
+                {
+                   
+                    case "▶️":
+                        index = (index == (evaled.length - 1)) ? index : index + 1;
+                        up();
+                        break;
+                    case "◀️":
+                        index = index ? index - 1 : index;
+                        up();
+                        break;
+                    case "⏮":
+                        index = 0;
+                        up();
+                        break;
+                    case "⏭":
+                        index = evaled.length - 1;
+                        up();
+                        break;
+                    case "🗑️":
+                        m.delete();
+                        collector.stop();
+                        break;
+                }
+            });
+
+}
