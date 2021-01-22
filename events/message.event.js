a842e4d0fe6db17817e11417f0b2caf7d242221c
nearlySplat <splatterxl@outlook.ie>
2021-01-22 17:35:17 +0000

    Update message.event.js

--- events/message.event.js
+++ events/message.event.js
@@ -73,7 +73,7 @@ module.exports = {
           this.client.responses.set(this.message.id, message);
         }
         bot.responses = this.client.responses;
-        this.util.depression(message, this.message, this)
+        setTimeout(() => this.util.depression(message, this.message, this), 5000)
         return message;
 
       },
