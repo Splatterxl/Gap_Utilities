ba6d8c64fde5d26e759c42f252729ee4ec457ad0
nearlySplat <splatterxl@outlook.ie>
2021-01-22 17:13:04 +0000

    Update paginate.js

--- misc/paginate.js
+++ misc/paginate.js
@@ -3,7 +3,7 @@ module.exports = async (pages, ctx, opts) => {
             if (!pages) return;
             if (!pages[0]) pages[0] = opts?.default || new Error("No default text specified");
             let m = opts?.respond ? await ctx.respond(pages[index], opts?.msgOptions) : await ctx.message.channel.send(pages[index], opts?.msgOptions)
-            function up() { m.edit(pages[index]); };
+            function up() { m.edit(pages[index], opts?.msgOptions); };
             pages.length > 1 ? ["⏮","◀️","▶️","⏭"].map(v => m.react(v)) : null;
             const collector = m.createReactionCollector((r, u) => (u.id === ctx.message.author.id));
             collector.on('collect', (r) =>
