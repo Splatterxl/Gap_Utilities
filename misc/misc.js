4df3a3342734f55f1edb680a1b6e6b0475c93c56
nearlySplat <splatterxl@outlook.ie>
2021-01-22 17:52:20 +0000

    Update misc.js

--- misc/misc.js
+++ misc/misc.js
@@ -1,6 +1,21 @@
 const files = require("fs").readdirSync("./misc").filter(v => v.endsWith(".js")).map(v => [v, require(`./${v}`)]);
 
-const object = {};
+let default = {
+  resolveCommand(string) {
+    if (!string || typeof string !== "string") throw new this.InternalError("INVALID_COMMAND", "An invalid command to resolve was specified.")
+    return cmds.find(v => v.help?.name == `>${string}` || v.help?.id == string || v.help?.aliases?.includes(string)) || null;
+  },
+  InternalError: class InternalError extends Error {
+    constructor (name, message) {
+      super("!!{text}!!");
+      this.name = name;
+      this.message = message;
+      this.stack = super.stack.replace(/!!\{text\}!!/g, message);
+    }
+  }
+};
+
+let object = default;
 
 files.forEach(v => object[v[0].replace(/\.js/g, "")] = v[1]);
 
