5f3eb20a7fcae548074b9086a0fec293ed80245e
nearlySplat <splatterxl@outlook.ie>
2021-01-23 13:18:23 +0000

    Update depression.js

--- misc/depression.js
+++ misc/depression.js
@@ -1,7 +1,7 @@
 module.exports = async (m, msg, ctx) => {
   if (!ctx) return;
   await m.react('🗑️');
-  const collector = m.createReactionCollector((r, u) => u.id == msg.author.id || ctx.guild.members.cache.get(u.id).permissions.has(ctx.Discord.Permissions.FLAGS.MANAGE_MESSAGES));
+  const collector = m.createReactionCollector((r, u) => !u.bot && (u.id == msg.author.id || ctx.guild.members.cache.get(u.id).permissions.has(ctx.Discord.Permissions.FLAGS.MANAGE_MESSAGES)));
   collector.on("collect", r => {
     if (r.emoji.name == '🗑️') m.delete();
   });
