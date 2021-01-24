bf9f71153d7139f3054f33fdaad13a963070fccd
nearlySplat <splatterxl@outlook.ie>
2021-01-24 02:12:38 +0000

    Update powerof.js

--- misc/powerof.js
+++ misc/powerof.js
@@ -1,3 +1,2 @@
-module.exports = async (num, msg) => {
+module.exports = async (num, msg) => 
   msg.channel.messages.cache.array().sort((a, b) => a.createdTimestamp - b.createdTimetamp).reverse()[num + 1]
-}
