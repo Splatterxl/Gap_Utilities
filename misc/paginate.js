796a2efa3b9b1aa18c345bd8ac64c88c69e256bd
nearlySplat <splatterxl@outlook.ie>
2021-01-24 14:09:49 +0000

    Update paginate.js

--- misc/paginate.js
+++ misc/paginate.js
@@ -1,4 +1,4 @@
-const { MessageEmbed, Message, MessageAttachment } = require('discord.js');
+const { MessageEmbed, Message, MessageAttachment } = require("discord.js");
 
 /**
  *
@@ -7,13 +7,13 @@ const { MessageEmbed, Message, MessageAttachment } = require('discord.js');
  *  message: Message,
  *  respond(content: string | MessageEmbed | MessageAttachment, options?): Promise<Message>
  * }} ctx
- * @param {*} opts
+ * @param {Object} [opts]
  */
-module.exports = async (pages, ctx, opts) => {
+module.exports = async (pages, ctx, opts = { respond: true }) => {
   let index = 0;
   if (!pages) return;
   if (!pages[0])
-    pages[0] = opts?.default || new Error('No default text specified');
+    pages[0] = opts?.default || new Error("No default text specified");
   let m =
     opts?.use || opts?.respond
       ? await ctx.respond(pages[index], opts?.msgOptions)
@@ -21,31 +21,37 @@ module.exports = async (pages, ctx, opts) => {
   function up() {
     m.edit(pages[index], opts?.msgOptions);
   }
-  pages.length > 1 ? ['⏮', '◀️', '⏹', '▶️', '⏭'].map(v => m.react(v)) : null;
-  const collector = m.createReactionCollector(
-    (r, u) => u.id === ctx.message.author.id
-  );
-  collector.on('collect', (r, u) => {
+  pages.length > 1 ? ["⏮", "◀️", "⏹", "▶️", "⏭"].map((v) => m.react(v)) : null;
+  const collector = m.createReactionCollector((r, u) => {
+    if (u.id === ctx.message.author.id) return true;
+    else {
+      r.users.remove(u.id).catch(() => {});
+      return false;
+    }
+  });
+  collector.on("collect", (r, u) => {
     r.users.remove(u.id).catch(() => {});
     switch (r.emoji.name) {
-      case '▶️':
+      case "▶️":
         index = index == pages.length - 1 ? index : index + 1;
         up();
         break;
-      case '◀️':
+      case "◀️":
         index = index ? index - 1 : index;
         up();
         break;
-      case '⏮':
+      case "⏮":
         index = 0;
         up();
         break;
-      case '⏭':
+      case "⏭":
         index = pages.length - 1;
         up();
         break;
-      case '⏹':
-        r.message.reactions.removeAll().catch(() => {})
-   }
+      case "⏹":
+        r.message.reactions.removeAll().catch(() => {});
+        collector.stop();
+        return;
+    }
   });
 };
