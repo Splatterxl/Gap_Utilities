41380c925bbf0bc249f7a7cc561a31e21b3d7d9a
nearlySplat <splatterxl@outlook.ie>
2021-01-25 03:59:41 +0000

    Update pinged.js

--- misc/pinged.js
+++ misc/pinged.js
@@ -21,10 +21,10 @@ module.exports = {
     ],"fuckoff":["sure","ok","lmao","what did i do wrong"]};
 
     if (msg.content.match(new RegExp(`^thanks <@!?${bot.user.id}>$`, 'g')))
-      return msg.channel.send(arrs.thanks[Math.floor(Math.random() * arr.length)]);
+      return msg.channel.send(arrs.thanks[Math.floor(Math.random() * arrs.thanks.length)]);
 
     if (msg.content.match(new RegExp(`^(s(hut)? ?t(he)? ?)?f(uck)?(( ?off)|( ?up?))? <@!?${bot.user.id}>$`, 'g')))
-      return msg.channel.send(arrs.fuckoff[Math.floor(Math.random() * arr.length)]);
+      return msg.channel.send(arrs.fuckoff[Math.floor(Math.random() * arrs.fuckoff.length)]);
 
     if (
       msg.content.includes(`<@${bot.user.id}>`) ||
