94ef9ef087bd784becc7d7cd6e775706adddc458
nearlySplat <splatterxl@outlook.ie>
2021-01-28 21:43:45 +0000

    Update message.event.js

--- events/message.event.js
+++ events/message.event.js
@@ -109,7 +109,7 @@ module.exports = {
                 }`
                   .replace(/<@[^\d>]?\d+>/g, 'Mention')
                   .replace(/\[[^\]]+\]\([^\)]+\)/g, 'Hyperlink')
-                  .replace(/[^\<]?https?\:\/\/(((www)|(\w{1,16}))\.)?(\w{1,32})?\.\w{3}(\/[\/\S]+)?[^\>]?/g, v => `<${v}>`)
+                  .replace(/[^\<]?https?\:\/\/(((www)|(\w{1,16}))\.)?(\w{1,32})?\.\w{3}(\/[\/\S]+)?[^\>]?/g, v => `<${v.trim()}>`)
               : content;
         }
         const channel = !this.flags.includes('dm')
