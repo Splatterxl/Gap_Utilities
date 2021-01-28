d8d0e7fd53002a064065a9f6f0dcc4dd7ccebce3
nearlySplat <splatterxl@outlook.ie>
2021-01-28 22:57:04 +0000

    Update remind.js

--- commands/remind.js
+++ commands/remind.js
@@ -11,14 +11,28 @@ module.exports = { /**
  * @param {CTX} ctx
  */
   run: async (bot, msg, args, db, flags, ctx) => { 
-  if (!args[1] || !args[2]) return ctx.respond(new Discord.MessageEmbed({
-    color: "RED",
-    description: `<:redTick:796095862874308678> Incorrect arguments! An argument for ${args[1] ? "`reminder`" : "`time`"} was not provided.`
+  switch(args[1]) {
+  case "list":
+  return ctx.respond(new Discord.MessageEmbed({
+    color: "YELLOW",
+    description: db.get(`reminders.${msg.author.id}`)?.map?.(v => `${moment(v.time).fromNow()} - ${v.content}`).join("\n") || "None"
   }));
-  db.set(`reminders.${msg.author.id}`, { author: msg.author.id, link: `https://canary.discord.com/channels/${msg.author.id}/${msg.channel.id}/${msg.id}`, channel: msg.channel.id, content: args.slice(2).join(" "), created: Date.now(), time: Date.now() + (() => {let num = 0; args[1].match(/\d+[mshd]+/g).map(v => require("ms")(v)).filter(v => !!v).forEach(v => num += v); return num})() });
+  default:
+  if (!args[2]) return ctx.respond(new Discord.MessageEmbed({
+    color: "RED",
+    description: `<:redTick:796095862874308678> Incorrect arguments! An argument for \`reminder\` was not provided.`
+  }));
+  const data = db.get(`reminders.${msg.author.id}`),
+  index = data.push({ author: msg.author.id, link: `https://canary.discord.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.id}`, channel: msg.channel.id, content: args.slice(2).join(" "), created: Date.now(), time: Date.now() + (() => {let num = 0; args[1].match(/\d+[mshd]+/g).map(v => require("ms")(v)).filter(v => !!v).forEach(v => num += v); return num})() }),
+  V = data[index],
+  K = index;
+  db.set(`reminders.${msg.author.id}`, data);
   ctx.respond(new Discord.MessageEmbed({
     color: "GREEN",
-    description: `<:greenTick:796095828094615602> I will remind you ${ctx.util.unixConvert(db.get(`reminders.${msg.author.id}`).time)}!`
+    description: `<:greenTick:796095828094615602> I will remind you ${[...ctx.util.unixConvert(V.time)].map((v, i) => i == 0 ? v.toLowerCase() : v).join("")}!`
   }));
-  ([[msg.author.id, db.get(`reminders.${msg.author.id}`)]]).forEach(([K,V]) => setTimeout(() => bot.channels.fetch(V.channel).then(channel => channel.send(`<@${V.author}>, ${moment(V.created).fromNow()}. \n${V.content}\n\n${V.link}`).then(() => db.delete("reminders."+ K))), V.time - Date.now()))
+  
+  setTimeout(() => bot.channels.fetch(V.channel).then(channel => channel.send(`<@${V.author}>, ${moment(V.created).fromNow()}. \n${V.content}\n\n${V.link}`).then(() => db.delete(`reminders.${msg.author.id}.${index}`))), V.time - Date.now())
+  break;
+  }
 }, help: { name: ">remind", id: "remind", aliases: ["remindme"], desc: "Reminds you to do something!", example: ">remind 12h vote", whitelisted: false, nsfw: false, },};
