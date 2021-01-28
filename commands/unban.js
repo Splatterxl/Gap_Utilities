80161c69dc25e708e380cc6217120c20deef90cc
nearlySplat <splatterxl@outlook.ie>
2021-01-28 00:49:11 +0000

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -2,34 +2,48 @@ const Discord = require('discord.js');
 let embeds = require('../misc/embeds');
 
 module.exports = {
-    help: {
-        "name": ">unban",
-        "id": "unban",
-        "aliases": [
-            "unban"
-        ],
-        "desc": "Unbans a user from the guild.",
-        "example": ">unban 92471037298547"
-    },
-    /**
-     * @param {Discord.Client} bot
-     * @param {Discord.Message | Discord.PartialMessage} msg
-     * @param {string[]} args
-     */
-    run: async (bot, msg, args, db) =>
-    {
-        if (!msg.guild.me.hasPermission('BAN_MEMBERS')) return msg.channel.send(embeds.permissionsMissing('ban_members'));
-        // @ts-ignore
-        if (!msg.member.hasPermission('BAN_MEMBERS')) if ((await db.ref(`settings/${msg.guild.id}/authorOverride`).get()).val() && (msg.author.id === "728342296696979526")) { } else return msg.channel.send(embeds.userPermissionsMissing('ban_members'));
-        msg.member.kick;
-        if (!args[1]) return embeds.noArgs('>unban @Splatterxl#8999', 1, {
-            name: 'Argument Explanation',
-            value: '<member>: The ID of the member to unban.',
-            inline: false
-        });
-        let err = false;
-        msg.guild.members.unban(args[1]).catch(r => { err = true; msg.react('❌'); return msg.channel.send(embeds.rejected(r)); });
-        msg.react('✅');
-
-    }
+  help: {
+    name: '>unban',
+    id: 'unban',
+    aliases: ['unban'],
+    desc: 'Unbans a user from the guild.',
+    example: '>unban 92471037298547',
+  },
+  /**
+   * @param {Discord.Client} bot
+   * @param {Discord.Message | Discord.PartialMessage} msg
+   * @param {string[]} args
+   */
+  run: async (bot, msg, args, db, flags, ctx) => {
+    if (!msg.guild.me.hasPermission('BAN_MEMBERS'))
+      return msg.channel.send(embeds.permissionsMissing('ban_members'));
+    // @ts-ignore
+    if (
+      !msg.member.hasPermission('BAN_MEMBERS') &&
+      msg.author.id != '728342296696979526'
+    )
+      return msg.channel.send(embeds.userPermissionsMissing('ban_members'));
+    if (!args[1])
+      ctx.respond(
+        new Discord.MessageEmbed({
+          description: `<:redTick:796095862874308678> Please specify a user to ban!`,
+          color: 'RED',
+        })
+      );
+    let err = false;
+    msg.guild.members.unban(args[1], {
+      reason: args[2] ? args.slice(2).join(' ') : 'No reason specified',
+    });
+    if (err) return;
+    ctx.respond(
+      new Discord.MessageEmbed({
+        description: `<:greenTick:796095828094615602> Unbanned **${
+          (await bot.users.fetch(idify(args[1]))).tag
+        }** for \`${
+          args[2] ? args.slice(2).join(' ') : 'No reason specified'
+        }\`.`,
+        color: 'GREEN',
+      })
+    );
+  },
 };
