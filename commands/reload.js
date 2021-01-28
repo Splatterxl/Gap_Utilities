1283bfee11f041f7d9c6f11da140585cd88a1341
nearlySplat <splatterxl@outlook.ie>
2021-01-28 02:43:07 +0000

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,5 +1,13 @@
-const Discord = require('discord.js');
-const embeds = require('../misc/embeds');
+const Discord = require('discord.js'),
+  embeds = require('../misc/embeds'),
+  fs = require("../misc/embeds")
+
+function formatBytes(bytes) {
+        if (bytes === 0) return '0 Bytes';
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        const i = Math.floor(Math.log(bytes) / Math.log(1024));
+        return `${parseFloat((bytes / Math.pow(1024, i)).toFixed(2))} ${sizes[i]}`;
+    }
 
 module.exports = {
   help: {
@@ -18,38 +26,8 @@ module.exports = {
    * @param {string[]} args
    */
   run: async (bot, msg, args, db, flags, ctx) => {
-    if (require('../whitelist').includes(msg.author.id)) {
-      if (!args) {
-        require('../events/commandLoader')(1);
-        msg.reply(
-          new Discord.MessageEmbed({
-            title: '<a:check:790313499225096213> Done!',
-            description: 'All commands have been reloaded!',
-          })
-        );
-      }
-      switch (args[1]?.toLowerCase()) {
-        case undefined:
-        case 'all':
-          require('../events/commandLoader')(1);
-          msg.reply(
-            new Discord.MessageEmbed({
-              title: '<:greenTick:796095828094615602> Done!',
-              description: 'All commands have been reloaded!',
-            })
-          );
-          break;
-        case 'braincells':
-          msg.reply(
-            new Discord.MessageEmbed({
-              title: '<:redTick:796095862874308678> An Error occurred',
-              description:
-                "```require('./commandLoader.js')('braincells')\n\n^^^^^^^^\n\nNo module 'braincells' found.```",
-            })
-          );
-          break;
-        default:
-          if (!flags.getObj().solo?.includes('event')) {
+    if (!require('../whitelist').includes(msg.author.id)) return ctx.respond(ctx.util.embeds.notWhiteslisted())      
+      
             if (
               global.cmds.find(
                 c => c.help?.id == args[1] || c.help?.aliases?.includes(args[1])
@@ -82,54 +60,19 @@ module.exports = {
                 }
                 msg.reply(
                   new Discord.MessageEmbed({
-                    title: '<:greenTick:796095828094615602> Done!',
-                    description: `\`${cmd}\` has been reloaded!`,
+                    color: 'GREEN',
+                    description: `<:greenTick:796095828094615602> \`${require.resolve(`./commands/${cmd}`).match(/commands\/[^\s]+/g)[0]}\` (\`${formatBytes(fs.statSync(`./commands/${"help"}.js`).size)}\`) has been reloaded!`,
                   })
                 );
               } catch (e) {}
             } else {
               msg.reply(
                 new Discord.MessageEmbed({
-                  title: '<:redTick:796095862874308678> An Error occurred',
-                  description: '```No such command exists!```',
+                  description: '<:redTick:796095862874308678> No such command exists!',
+                  color: "RED"
                 })
               );
             }
-          } else {
-            if (
-              global.events.find(
-                c => c.help?.id == args[1] || c.help?.aliases?.includes(args[1])
-              ) ||
-              flags.includes('--force')
-            ) {
-              try {
-                let cmd = global.events.get(args[1]);
-                delete require.cache[require.resolve(`../events/${cmd}.event`)];
-                global.events.delete(args[1]);
-                // @ts-ignore
-                global.cmds.set(
-                  args[1],
-                  require(`../events/${args[1]}.event`).prototype
-                    ? new (require(`../events/${args[1]}.event`))()
-                    : require(`../events/${args[1]}.event`)
-                );
-                msg.reply(
-                  new Discord.MessageEmbed({
-                    title: '<:greenTick:796095828094615602> Done!',
-                    description: `\`${args[1]}\` has been reloaded!`,
-                  })
-                );
-              } catch (e) {}
-            } else {
-              ctx.respond(
-                new Discord.MessageEmbed({
-                  title: '<:redTick:796095862874308678> An Error occurred',
-                  description: '```No such event exists!```',
-                })
-              );
-            }
-          }
-      }
-    } else return msg.reply(embeds.notWhitelisted());
+         
   },
 };
