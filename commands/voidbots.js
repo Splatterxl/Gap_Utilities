d88d5eeba715b42a2b26650f2b4818f30dde70a7
nearlySplat <splatterxl@outlook.ie>
2021-01-28 16:20:21 +0000

    Update voidbots.js

--- commands/voidbots.js
+++ commands/voidbots.js
@@ -25,7 +25,7 @@ module.exports = {
       })
     );
     else if (flags.includes("voted") || flags.includes("v")) {
-      const data = await ctx.util.vbapi.voted(ctx.client.user.id, ctx.util.idify(args[1]) ?? msg.author.id)
+      const data = await ctx.util.vbapi.voted(ctx.client.user.id, ctx.util.idify(args[1]) || msg.author.id)
       return ctx.respond(new Discord.MessageEmbed({
         color: "YELLOW",
         description: `**Has Voted**: \`${data.voted}\`\n${data.voted ? `**Voted At** \`${ctx.util.unixConvert((new Date(data.votedAt)).getTime())}\`\n**Next Vote**:\n⇒ __Date__: \`${ctx.util.unixConvert(Date.now() + data.nextVote.ms)}\`` : ""}`
