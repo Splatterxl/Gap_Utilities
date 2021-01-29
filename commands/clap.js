75a9c0d4ca673462f4fbe4a890b129ea189d2969
nearlySplat <splatterxl@outlook.ie>
2021-01-29 17:42:51 +0000

    Create clap.js

---
+++ commands/clap.js
@@ -0,0 +1,11 @@
+module.exports = {
+  help: {
+    name: ">clap",
+    id: "clap",
+    desc: "👏 Make 👏 stuff 👏 like 👏 this 👏",
+    aliases: [ "👏" ],
+    category: "fun",
+    whitelisted: false
+  },
+  run: (bot, msg, args, db, flags, ctx) => ctx.respond(args.slice(1).join(" 👏 "))
+}
