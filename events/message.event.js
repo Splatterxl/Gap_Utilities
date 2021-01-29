593bdfcb42476ed487be8540e3b1ee67d6adb686
nearlySplat <splatterxl@outlook.ie>
2021-01-29 02:05:04 +0000

    Update message.event.js

--- events/message.event.js
+++ events/message.event.js
@@ -259,7 +259,7 @@ module.exports = {
         .map(v => [...v].reverse().join('')),
       { type: 'end' }
     ); */
-    const prefixes = bot.user.id != "784833064400191509" ? [ db.get(`settings.g${msg.guild.id}.prefixes`), db.get(`settings.u${msg.author.id}.prefixes`), [ `${bot.user}` ] ] : [ [ "eb;" ] ];
+    ;const prefixes = bot.user.id != "784833064400191509" ? [ db.get(`settings.g${msg.guild.id}.prefixes`), db.get(`settings.u${msg.author.id}.prefixes`), [ `${bot.user}` ] ] : [ [ "eb;" ] ];
     prefixes.forEach(v => v?.map(v => msg.content.startsWith(v) ? parseCmd(msg.content, v.length, v) : null))
     if (msg.author.discriminator === '0000') return;
     // @ts-ignore
