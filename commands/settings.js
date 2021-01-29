380694759eee0a3f4195264d5834310ca8c75b0d
nearlySplat <splatterxl@outlook.ie>
2021-01-29 01:56:09 +0000

    Update settings.js

--- commands/settings.js
+++ commands/settings.js
@@ -25,19 +25,28 @@ module.exports = {
      */
     run: async (bot, msg, args, db, flags, ctx) =>
     {
-        if (!msg.member.hasPermission('MANAGE_GUILD'))
-            // @ts-ignore
-            if (msg.author.id != "728342296696979526")
+        if (args[2] == "guild" && (!msg.member.hasPermission('MANAGE_GUILD') && msg.author.id != "728342296696979526"))
             {
                 ctx.respond(embeds.userPermissionsMissing('manage_guild'))
             };
-        if (!args[1] || !args[2]) return ctx.respond("You need to provide a key and a value. Example: `settings prefix e;`");
+        if (!args[1] || !args[2] || !args[3]) return ctx.respond("You need to provide a type, key and value. Example: `settings addprefix user e;`");
         switch (args[1].toLowerCase())
             {
-                case 'prefix':
-                    
-                    db.set(`settings.g${msg.guild.id}.prefix`, args.slice(2).join(' '));
-                    return ctx.respond('Server prefix changed to `' + args.slice(2).join(' '))
+                case 'addprefix':
+                    let data = db.get(`settings.${args[2] == "user" ? "u" : "g"}${msg.guild.id}.prefixes`);
+                    if (!(data ?? false)) { data = []; db.set(`settings.${args[2] == "user" ? "u" : "g"}${msg.guild.id}.prefixes`, data) }
+                    if (data.includes(args.slice(3).join(" "))) return ctx.respond("Prefix already exists.")
+                    let index = data.push(args.slice(3).join(" ")) - 1,
+                      value: data[index];
+                    db.set(`settings.${args[2] == "user" ? "u" : "g"}${msg.guild.id}.prefixes`, data)
+                    return ctx.respond('Prefix `' + args.slice(3).join(' ') + '` was added to the database.');
+                 case 'rmprefix':
+                    let data = db.get(`settings.${args[2] == "user" ? "u" : "g"}${msg.guild.id}.prefixes`);
+                    if (!(data ?? false)) { data = []; db.set(`settings.${args[2] == "user" ? "u" : "g"}${msg.guild.id}.prefixes`, data) }
+                    if (!data.includes(args.slice(3).join(" "))) return ctx.respond("Prefix does not exist.")
+                    data = data.splice(data.indexOf(args.slice(3).join(" ")), 1)
+                    db.set(`settings.${args[2] == "user" ? "u" : "g"}${msg.guild.id}.prefixes`, data)
+                    return ctx.respond('Prefix `' + args.slice(3).join(' ') + '` was removed from the database.');
             }
     }
 };
