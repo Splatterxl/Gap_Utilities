38240fcfd2abcb7092eb384665812a715735984c
nearlySplat <splatterxl@outlook.ie>
2021-01-29 23:59:15 +0000

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -22,15 +22,16 @@ module.exports = {
    * @param {string[]} args
    */
   run: async (bot, msg, args, db, flags, ctx) => {
-    if (!ctx.args[3]) return ctx.respond("I need a channel, question and 2 to 9 options.")
-    const matchedOpts = ctx.args.slice(3).join(" ").match(/((\"[^"]+\")|(\S+))/g).map(v => v.replace(/\"/g, "")),
+    if (!ctx.args[2]) return ctx.respond("I need a channel, question and 2 to 9 options.")
+    const matchedOpts = ctx.args.slice(2).join(" ").match(/((\"[^"]+\")|(\S+))/g).map(v => v.replace(/\"/g, "")),
       question = matchedOpts[0],
       answers = matchedOpts.slice(1),
       letters = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]
     if (answers.length <= 1 || args.length >= 10) return ctx.respond("I need a channel, question and 2 to 9 options.");
     ctx.message.mentions?.channels?.first()?.send?.(new Discord.MessageEmbed({
-      author: { image_url: ctx.message.author.avatarURL({ dynamic: true }), text: `Poll by ${ctx.message.author.tag}` },
-      description: `**${question}**\n\n${answers.map((v, i) => `${letters[i]}: ${v}`).join("\n")}`
+      footer: { icon_url: ctx.message.author.avatarURL({ dynamic: true }), text: `Poll by ${ctx.message.author.tag}` },
+      title: question,
+      description: `${answers.map((v, i) => `**Option ${i + 1}**: ${v}`).join("\n")}`
     })).then(v => { letters.slice(0, answers.length).map(l => v.react(l)); ctx.respond(`I have created a poll in ${ctx.message.mentions?.channels?.first?.()?.toString()}`) }) || ctx.respond("Something went wrong, try again?")
   }
 };
