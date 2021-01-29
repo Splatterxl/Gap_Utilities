6deeb3c5288515aa8a4dcbb44ee87277ca16cd35
nearlySplat <splatterxl@outlook.ie>
2021-01-29 19:33:11 +0000

    Update guildMemberAdd.event.js

--- events/guildMemberAdd.event.js
+++ events/guildMemberAdd.event.js
@@ -2,8 +2,7 @@ const Discord = require("discord.js")
 
 module.exports.run = async (member, db) => {
   if (!db.get(`settings.g${member.guild.id}.joinNotifs`)) return;
-  const client = member.guild.channels.cache.filter(v => v instanceof Discord.TextChannel).first().messages.cache.first().client,
-    channelId = db.get(`settings.g${member.guild.id}.joinNotifs`),
+  const channelId = db.get(`settings.g${member.guild.id}.joinNotifs`),
     channel = await client.channels.fetch(channelId).catch(e => null);
   channel.send(`**${member.user.tag}** joined! Welcome!`).catch(e => null)
 }
