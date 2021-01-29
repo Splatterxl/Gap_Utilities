c00e676932d76a261c8f7edc30fb1e59dcfdcb71
nearlySplat <splatterxl@outlook.ie>
2021-01-29 22:54:25 +0000

    Update exec.js

--- misc/exec.js
+++ misc/exec.js
@@ -3,12 +3,11 @@ const childProcess = require("child_process")
 // Thanks to @Zytekaron for the code
 function exec(commands) {
     return new Promise(resolve => {
-        let buf = [], resolved = false
-  
+        let buf = [], killed = false 
         const child = childProcess.spawn(commands.join(' && '), {
             shell: true
         });
-        const timeout = setTimeout(() => { child.kill("SIGTERM"); }, 30000)
+        const timeout = setTimeout(() => { child.kill("SIGTERM"); killed = true }, 30000)
 
         child.stdout.on('data', data => {
             buf.push(data.toString());
@@ -19,14 +18,14 @@ function exec(commands) {
         });
 
         child.on('exit', exitCode => {
+            if (killed) return;
             buf.push('Exited with code: ');
             buf.push(exitCode);
-            resolved = true;
             clearTimeout(timeout)
             resolve(buf.join(''));
         });
         child.on('close', signal => {
-            buf.push('Exited with signal: ');
+            buf.push('\nExited with signal: ');
             buf.push(signal);
             resolved = true;
             resolve(buf.join(''));
