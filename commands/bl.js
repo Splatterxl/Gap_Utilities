f80b54681e8dfadb7c83a3cf9fa4f0485dba424a
nearlySplat <splatterxl@outlook.ie>
2021-01-30 03:53:31 +0000

    Create bl.js

---
+++ commands/bl.js
@@ -0,0 +1,17 @@
+module.exports.run = async (a, b, c, d, e, ctx) => {
+  if (!ctx.args[2]) return ctx.respond("Incorrect usage; should have at least two parameters (`{type: string} {user: ResolveableUser}`)");
+  let usr = ((await ctx.client.users.fetch(args[1], true).catch(e => null)) ?? ctx.guild.members.cache.find(v => v.user.id == args[1] || v.displayName == args[1] || v.displayName.startsWith(args[1]) || v.displayName.includes(args[1]) || v.user.tag == args[1] || v.user.username == args[1] || v.user.username.startsWith(args[1]) || v.user.username.includes(args[1])).user) ?? ctx.message.author;
+  usr = usr?.id
+  switch (args[1]) {
+    case "check":
+      const data = db.get(`blacklist.${usr}`);
+      return ctx.respond(`**${ctx.client.users.cache.get(usr).tag}** is ${data ? "" : "__not__ "}blacklisted${data ? "for `" + data + "`." : ""}.`)
+    case "add":
+      db.set(`blacklist.${usr}`, args.slice(2).join(" ") || "No reason specified.");
+      return ctx.respond(`Successfully blacklisted **${ctx.client.users.cache.get(usr).tag}**.`)
+    case "remove":
+    case "rm":
+      db.delete(`blacklist.${usr}`);
+      return ctx.respond(`Successfully unblacklisted **${ctx.client.users.cache.get(usr).tag}**.`)
+  }
+}
