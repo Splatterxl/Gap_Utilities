d8fdb7a2f8bcc12fc9d1d4ca98e8ed4cd3f97460
nearlySplat <splatterxl@outlook.ie>
2021-01-30 16:50:45 +0000

    Update bl.js

--- commands/bl.js
+++ commands/bl.js
@@ -1,18 +1,25 @@
 module.exports.run = async (a, b, args, d, e, ctx) => {
-  if (!ctx.args[2]) return ctx.respond("Incorrect usage; should have at least two parameters (`{type: string} {user: ResolveableUser}`)");
-  let usr = ((await ctx.client.users.fetch(args[1], true).catch(e => null)) ?? ctx.guild.members.cache.find(v => v.user.id == args[2] || v.displayName == args[2] || v.displayName.startsWith(args[2]) || v.displayName.includes(args[2]) || v.user.tag == args[2] || v.user.username == args[2] || v.user.username.startsWith(args[2]) || v.user.username.includes(args[2]))?.user) ?? ctx.message.author;
+  if (!ctx.args[2]) return ctx.respond(ctx.util.embeds.errorEmbed("Incorrect usage; should have at least two parameters (`{type: string} {user: ResolveableUser}`)"));
+  let target = args.slice(2).join(" ").toLowerCase(),
+  assert = (data) => data.toLowerCase() == target || data.toLowerCase().startsWith(target) || data.toLowerCase().includes(target)
+  usr = ((await cctx.client.users.fetch(target, true).catch(e => null)) ?? ctx.guild.members.cache.find(v => assert(v.user.id) || assert(displayName) || assert(v.user.tag) || assert(v.user.username))?.user);
+  if (!usr) return ctx.respond(ctx.util.embeds.errorEmbed("I could not find that user!"))
   usr = usr?.id
   switch (args[1]) {
     case "check":
       const data = ctx.db.get(`blacklist.${usr}`);
       return ctx.respond(`**${ctx.client.users.cache.get(usr).tag}** is ${data ? "" : "__not__ "}blacklisted${data ? "for `" + data + "`." : ""}.`)
-    case "add":
-      ctx.db.set(`blacklist.${usr}`, ctx.args.slice(3).join(" ") == "" ? "No reason specified." : ctx.args.slice(3).join(" "));
-      return ctx.respond(`Successfully blacklisted **${ctx.client.users.cache.get(usr).tag}**.`)
+    case "add"
+      ctx.channel.send(ctx.util.embeds.collectorEmbed(`What reason should I add **${ctx.client.users.cache.get(usr).tag}** to the blacklist for?`));
+      try {
+      const reason = (await ctx.channel.awaitMessages((m) => m.author.id == ctx.message.author.id, { max: 1, time: 6000, errors: ["time"] })).first().content
+      } catch { return ctx.respond(ctx.util.embeds.errorEmbed("You didn't send a message in time!")) }
+      ctx.db.set(`blacklist.${usr}`, reason);
+      return ctx.respond(ctx.util.embeds.okEmbed(`Successfully blacklisted **${ctx.client.users.cache.get(usr).tag}**.`))
     case "remove":
     case "rm":
       ctx.db.delete(`blacklist.${usr}`);
-      return ctx.respond(`Successfully unblacklisted **${ctx.client.users.cache.get(usr).tag}**.`)
+      return ctx.respond(ctx.util.embeds.okEmbed(`Successfully unblacklisted **${ctx.client.users.cache.get(usr).tag}**.`))
   }
 }
 module.exports.help = {
