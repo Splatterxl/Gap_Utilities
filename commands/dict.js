fefac6ca5296d9736133a317ab00947f7e8dd1b0
nearlySplat <splatterxl@outlook.ie>
2021-01-30 23:38:11 +0000

    Update dict.js

--- commands/dict.js
+++ commands/dict.js
@@ -33,7 +33,7 @@ module.exports = {
                   title: `Definitions for ${body.word}`,
                   description: `**Phonetics**: ${body.phonetics?.map(v => `\`${v.text}\``).join(" | ")}\n**Meanings found**: ${body.meanings?.length}\n**Parts of speech**: ${body.meanings?.map(v => v.partOfSpeech)?.map(v => `\`${v}\``).join(" | ")}`
                 }),
-                ...(body.meanings?.map((v, i, a) => new Discord.MessageEmbed().setTitle(`Definitions for ${body.word}`).setDescription(`**Part of speech**: ${v.partOfSpeech}\n**Definitions:**\n${v.definitons?.map(v => `  **Definition**: ${v.definiton}\n  **Example**: ${v.example}`)}`).join("\n")) ?? [ ctx.util.embeds.neutralEmbed("Nothing here UwU", false) ])
+                ...(body.meanings?.map((v, i, a) => new Discord.MessageEmbed().setTitle(`Definitions for ${body.word}`).setDescription(`**Part of speech**: ${v.partOfSpeech}\n**Definitions:**\n${v.definitons?.map(v => `  **Definition**: ${v.definiton}\n  **Example**: ${v.example}`).join("\n")}`)) ?? [ ctx.util.embeds.neutralEmbed("Nothing here UwU", false) ])
               ], ctx)
             });
     }
