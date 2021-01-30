6b1229b1cef8aaed20febae8e10cf83b49dc20b9
nearlySplat <splatterxl@outlook.ie>
2021-01-30 20:29:13 +0000

    Update perms.js

--- commands/perms.js
+++ commands/perms.js
@@ -23,7 +23,7 @@ module.exports = {
       title: `${member.user.tag}'s Permissions`,
       description: new Permissions(Permissions.ALL)
         .toArray()
-        .map((perm) => `\`${perm.replace(/_/g, "").replace(/\b\w/g, v => v.toUpperCase())}\` | ${member.permissions.has(perm) ? '<:greenTick:796095828094615602>' : "<:redTick:796095862874308678>"}`)
+        .map((perm) => `\`${perm.replace(/_/g, " ").replace(/[^\b]\w/g, v => v.toLowerCase())}\` | ${member.permissions.has(perm) ? '<:greenTick:796095828094615602>' : "<:redTick:796095862874308678>"}`)
         .join("\n"),
     });
     msg.channel.send(_);
