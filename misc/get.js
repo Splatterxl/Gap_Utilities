24b0f7de9ddc1d72168246a9077c7dd7f725817f
nearlySplat <splatterxl@outlook.ie>
2021-01-30 20:40:07 +0000

    Update get.js

--- misc/get.js
+++ misc/get.js
@@ -1,4 +1,4 @@
 const assert = (data, target) => data.toLowerCase() == target || data.toLowerCase().startsWith(target) || data.toLowerCase().includes(target)
 
-module.exports.member = async (ctx, target) => (await ctx.guild.members.fetch(target, true).catch(e => null)) ?? ctx.guild.members.cache.find(v => assert(v.user.id, target) || assert(v.displayName, target) || assert(v.user.tag, target) || assert(v.user.username, target));
-module.exports.user = async (ctx, target) => (await ctx.client.users.fetch(target, true).catch(e => null)) ?? ctx.client.users.cache.find(v => assert(v.id, target) || assert(v.tag, target) || assert(v.username, target));
+module.exports.member = async (ctx, target) => (await ctx.guild.members.fetch(target.replace(/[^\d]+/g, ""), true).catch(e => null)) ?? ctx.guild.members.cache.find(v => assert(v.user.id, target.replace(/[^\d]+/g, "")) || assert(v.displayName, target) || assert(v.user.tag, target) || assert(v.user.username, target));
+module.exports.user = async (ctx, target) => (await ctx.client.users.fetch(target.replace(/[^\d]+/g, ""), true).catch(e => null)) ?? ctx.client.users.cache.find(v => assert(v.id, target.replace(/[^\d]+/g, "")) || assert(v.tag, target) || assert(v.username, target));
