6c6d03a7715fdcb6580d46d6e43ced0e35b83d31
nearlySplat <splatterxl@outlook.ie>
2021-02-01 16:45:55 +0000

    Update slowmode.js

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -19,7 +19,7 @@ module.exports = {
   run: async (bot, msg, args, db, flags, ctx) => {
     if (!ctx.message.member.permissions.has("MANAGE_CHANNELS") && !ctx.isOwner) return ctx.respond(ctx.util.embeds.userPermissionsMissing("MANAGE_CHANNELS"));
     if (!args[1]) return ctx.respond(ctx.util.embeds.errorEmbed("An argument for `rateLimit` was not supplied."));
-    const time = args[1].match(/\d+[msdwymh]?[s]?/)?.map(v => !v.match(/\w+/g) ? `${v}ms` : v).map(v => ms(v)).join("");
+    const time = (()=>{let num = 0;args[1].match(/\d+[msdwymh]?[s]?/)?.map(v => !v.match(/\w+/g) ? `${v}ms` : v).map(v => ms(v)).forEach(v => num += v);return num;})();
     if (!time || isNaN(parseInt(time))) return;
     ctx.channel.setRateLimitPerUser(parseInt(time)).then(() => ctx.respond(ctx.util.embeds.okEmbed(`I have set the channel rate limit to \`${time}\` (${args[1]}).`))).catch(e => ctx.respond(ctx.util.embeds.errorEmbed("I couldn't set the channel rate limit!")))
   },
