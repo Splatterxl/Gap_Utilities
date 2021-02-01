7701e41591f2a220923b7d9c62dc61cb7e303fd2
nearlySplat <splatterxl@outlook.ie>
2021-02-01 22:53:36 +0000

    Update uwu.js

--- commands/uwu.js
+++ commands/uwu.js
@@ -1,3 +1,13 @@
 const uwu = require("uwu-js")
 
-module.exports.run = (bot, msg, args) => msg.reply(uwu(args.slice(1).join(" ")))
+module.exports.run = (bot, msg, args, db, flags, ctx) => ctx.respond(uwu(args.slice(1).join(" ")));
+
+module.exports.help = {
+  name: ">uwu",
+  id: "uwu",
+  aliases: ["owo"],
+  desc: uwu("UwU-ifies text!"),
+  category: "fun",
+  whitelisted: false,
+  example: ">uwu owo UwU TwT"
+}
