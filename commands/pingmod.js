1820a1d2e6b00d195c63b3419c98ac4fcc310b03
nearlySplat <splatterxl@outlook.ie>
2021-02-07 21:47:43 +0000

    Create pingmod.js

---
+++ commands/pingmod.js
@@ -0,0 +1,22 @@
+ctx.channel.send("Are you sure you want to ping a mod?")
+ctx.channel
+  .awaitMessages((m) => m.author.id == ctx.message.author.id, { max: 1, time: 60000, errors: ["time"] })
+  .then((msgs) => {
+    if (!["yes","y","yis","accept","true","mhm","sure"].includes(msgs.first().content.toLowerCase().trim())) return ctx.channel.send("Aborted.")
+    const ids = ctx.guild.members.cache
+      .filter(
+        ({ permissions, user }) =>
+          permissions.any([
+            "BAN_MEMBERS",
+            "KICK_MEMBERS",
+            "MANAGE_MESSAGES",
+            "MANAGE_WEBHOOKS",
+          ]) &&
+          !user.bot &&
+          ["online", "dnd", "idle"].includes(
+            Object.values(user.presence.clientStatus ?? {})[0]
+          )
+      )
+      .map(({ user: { id, tag } }) => id);
+    return ctx.channel.send(`<@${ids[Math.floor(Math.random() * ids.length)]}>`, ctx.util.embeds.neutralEmbed(`${ctx.message.author.toString()} has mentioned you for \`${args.slice(1).join(" ")}\``));
+  });
