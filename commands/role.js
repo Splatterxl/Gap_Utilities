0bc0c558e2805a9f2458bd41e2f19c3455c91766
nearlySplat <splatterxl@outlook.ie>
2021-02-07 15:09:32 +0000

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -1,18 +1,18 @@
-const Discord = require('discord.js');
-const idify = require('../misc/idify');
-let embeds = require('../misc/embeds');
+const Discord = require("discord.js");
+const idify = require("../misc/idify");
+let embeds = require("../misc/embeds");
 
 module.exports = {
   help: {
-    name: '>role',
-    id: 'role',
-    aliases: ['roles'],
-    desc: 'Modifies the roles of a member.',
-    example: '>role add 7o70232718339604522 Member',
-    category: 'moderation',
+    name: ">role",
+    id: "role",
+    aliases: ["roles"],
+    desc: "Modifies the roles of a member.",
+    example: ">role add 7o70232718339604522 Member",
+    category: "moderation",
     whitelisted: false,
     permLvl: 4,
-    requiredPerms: [ "MANAGE_ROLES" ]
+    requiredPerm: ["MANAGE_ROLES"],
   },
   /**
    * @param {Discord.Client} bot
@@ -21,82 +21,96 @@ module.exports = {
    * @param {firebase.default.database.Database} db
    */
   run: async (bot, msg, args, db, flags, ctx) => {
-    if (!(await ctx.util.get
-      .member(ctx, args[2])).manageable) return ctx.respond(ctx.util.embeds.errorEmbed("I can't modify the roles of that user!"))
+    if (!(await ctx.util.get.member(ctx, args[2])).manageable)
+      return ctx.respond(
+        ctx.util.embeds.errorEmbed("I can't modify the roles of that user!")
+      );
     args = ctx.args;
-let name;
-let usr;
-switch (args[1]) {
-  case "a":
-  case "add":
-    name = ctx.guild.roles.cache.find(
-      (v) =>
-        v.name.toLowerCase() == args.slice(3).join(" ").toLowerCase() ||
-        v.name.startsWith(args.slice(3).join(" ").toLowerCase())
-    )?.name;
-    ctx.util.get
-      .member(ctx, args[2])
-      .then(async (v) =>
-        v?.roles.add(
-          (await v.guild.roles
-            .fetch(args[3])
-            .catch((e) => null)
-            .then((v) => {
-              name = v.name;
-              return v;
-            })?.id) ??
-            v.guild.roles.cache.find(
-              (v) =>
-                v.name.toLowerCase() == args.slice(3).join(" ").toLowerCase() ||
-                v.name.startsWith(args.slice(3).join(" ").toLowerCase())
-            ).id
-        )
-      )
-      .then((v) => {
-        usr = v.user.tag;
-        ctx.channel.send(
-          ctx.util.embeds.okEmbed(
-            `Updated **${usr}**: \`\`\`diff\n+ ${name}\n\`\`\``
+    let name;
+    let usr;
+    switch (args[1]) {
+      case "a":
+      case "add":
+        name = ctx.guild.roles.cache.find(
+          (v) =>
+            v.name.toLowerCase() == args.slice(3).join(" ").toLowerCase() ||
+            v.name.startsWith(args.slice(3).join(" ").toLowerCase())
+        )?.name;
+        ctx.util.get
+          .member(ctx, args[2])
+          .then(async (v) =>
+            v?.roles.add(
+              (await v.guild.roles
+                .fetch(args[3])
+                .catch((e) => null)
+                .then((v) => {
+                  name = v.name;
+                  return v;
+                })?.id) ??
+                v.guild.roles.cache.find(
+                  (v) =>
+                    v.name.toLowerCase() ==
+                      args.slice(3).join(" ").toLowerCase() ||
+                    v.name.startsWith(args.slice(3).join(" ").toLowerCase())
+                ).id
+            )
           )
-        );
-      });
-    break;
-  case "r":
-  case "rm":
-  case "remove":
-    name = ctx.guild.roles.cache.find(
-      (v) =>
-        v.name.toLowerCase() == args.slice(3).join(" ").toLowerCase() ||
-        v.name.startsWith(args.slice(3).join(" ").toLowerCase())
-    )?.name;
-    ctx.util.get
-      .member(ctx, args[2])
-      .then(async (v) =>
-        v?.roles.remove(
-          (await v.guild.roles
-            .fetch(args[3])
-            .catch((e) => null)
-            .then((v) => {
-              name = v.name;
-              return v;
-            })?.id) ??
-            v.guild.roles.cache.find(
-              (v) =>
-                v.name.toLowerCase() == args.slice(3).join(" ").toLowerCase() ||
-                v.name.startsWith(args.slice(3).join(" ").toLowerCase()) || v.id == args[3]
-            )?.id
-        )
-      )
-      .then((v) => {
-        usr = v.user.tag;
-        ctx.channel.send(
-          ctx.util.embeds.okEmbed(
-            `Updated **${usr}**: \`\`\`diff\n- ${name}\n\`\`\``
+          .then((v) => {
+            usr = v.user.tag;
+            ctx.channel.send(
+              ctx.util.embeds.okEmbed(
+                `Updated **${usr}**: \`\`\`diff\n+ ${name}\n\`\`\``
+              )
+            );
+          });
+        break;
+      case "r":
+      case "rm":
+      case "remove":
+        name = ctx.guild.roles.cache.find(
+          (v) =>
+            v.name.toLowerCase() == args.slice(3).join(" ").toLowerCase() ||
+            v.name.startsWith(args.slice(3).join(" ").toLowerCase())
+        )?.name;
+        ctx.util.get
+          .member(ctx, args[2])
+          .then(async (v) =>
+            v?.roles.remove(
+              (await v.guild.roles
+                .fetch(args[3])
+                .catch((e) => null)
+                .then((v) => {
+                  name = v.name;
+                  return v;
+                })?.id) ??
+                v.guild.roles.cache.find(
+                  (v) =>
+                    v.name.toLowerCase() ==
+                      args.slice(3).join(" ").toLowerCase() ||
+                    v.name.startsWith(args.slice(3).join(" ").toLowerCase()) ||
+                    v.id == args[3]
+                )?.id
+            )
           )
-        );
-      });
-    break;
-}
-
+          .then((v) => {
+            usr = v.user.tag;
+            ctx.channel.send(
+              ctx.util.embeds.okEmbed(
+                `Updated **${usr}**: \`\`\`diff\n- ${name}\n\`\`\``
+              )
+            );
+          });
+        break;
+      case "list":
+        ctx.util.get.member(ctx, args[2]).then(async (v) => {
+          ctx.respond(
+            `**${v.user?.tag}**'s roles:\n\n` +
+              ctx.util.embeds.neutralEmbed(
+                v.roles.cache.map((v) => v.toString()).join(", "),
+                false
+              )
+          );
+        });
+    }
   },
 };
