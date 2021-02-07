c237e28c2d756ee548877fe2e9a3e764087669c1
nearlySplat <splatterxl@outlook.ie>
2021-02-07 18:06:07 +0000

    Update flags.js

--- misc/flags.js
+++ misc/flags.js
@@ -14,7 +14,7 @@ module.exports = class Flags {
   /**
    * @private
    */
-  _regexp = /\b(-{1,2}\b[\w\S]+\b((=|:)?\s*\b(([\w\S\d]+|"[^"]+")))?\b)/gi;
+  _regexp = /-{1,2}\b[\w\S]+\b((=|:)?\s*\b(([\w\S\d]+|"[^"]+")))?\b/gi;
   /**
    *
    * @param {string} str
