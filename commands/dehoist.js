fca1fca9c6c20f5b319187374d731bd96d7f611a
nearlySplat <splatterxl@outlook.ie>
2021-02-08 13:01:51 +0000

    Update dehoist.js

--- commands/dehoist.js
+++ commands/dehoist.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (a, b, args, d, e, ctx) => {
-  (ctx.args.slice(1).length ? ctx.args.slice(1) : [ctx.message.author.id]).forEach(v => ctx.util.get.member(ctx, v).then(v => {let name = v.displayName; v.setNickname?.(msg.member.displayName.replace(/(^[!.?,\-\/:;()€&@”[\]{}£\#%\^\*\+=•¥’!\$\?\s]+|(^[\S.]$))/g, "").trim().replace(/\s+/g, " ")).then(m => ctx.channel.send(`Dehoisted \`${name}\` to \`${m.displayName}\`.`))}));
+  (ctx.args.slice(1).length ? ctx.args.slice(1) : [ctx.message.author.id]).forEach(v => ctx.util.get.member(ctx, v).then(v => {let name = v.displayName; v.setNickname?.(v.displayName.replace(/(^[!.?,\-\/:;()€&@”[\]{}£\#%\^\*\+=•¥’!\$\?\s]+|(^[\S.]$))/g, "").trim().replace(/\s+/g, " ")).then(m => ctx.channel.send(`Dehoisted \`${name}\` to \`${m.displayName}\`.`))}));
 };
 module.exports.help = {
   name: '>dehoist',
