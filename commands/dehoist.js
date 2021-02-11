d2ef1b4e20506d9707b65fbc86b0f18693c069eb
nearlySplat <splatterxl@outlook.ie>
2021-02-11 18:33:40 +0000

    Update dehoist.js

--- commands/dehoist.js
+++ commands/dehoist.js
@@ -1,39 +1,59 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports.run = async (a, b, args, d, e, ctx) => {
-  if (ctx.args[1]?.toLowerCase?.() === "all") return dehoistAll(ctx.guild.members.cache.map(v => v.user.id)).then(s => ctx.respond(`Dehoisted ${s.length} members.`)).catch(([errs, s]) => ctx.respond(`Dehoisted ${s.length} members, failed ${errs.length}.`))
-  dehoistAll((ctx.args.slice(1).length ? ctx.args.slice(1) : [ctx.message.author.id])).then(v => ctx.respond(v.map(`Dehoisted \`${v[0]}\` to \`${v[1]}\``).join("\n"))).catch(([errs]) => ctx.respond(`Couldn't dehoist ${errs.map(([name]) => `\`${name}\``).join(", ")}`));
-  async function dehoistAll (v) {
-    let successes = [], errs = []
+  if (ctx.args[1]?.toLowerCase?.() === "all")
+    return dehoistAll(ctx.guild.members.cache.map((v) => v.user.id))
+      .then((s) => ctx.respond(`Dehoisted ${s.length} members.`))
+      .catch(([errs, s]) =>
+        ctx.respond(`Dehoisted ${s.length} members, failed ${errs.length}.`)
+      );
+  dehoistAll(
+    ctx.args.slice(1).length ? ctx.args.slice(1) : [ctx.message.author.id]
+  )
+    .then((v) =>
+      ctx.respond(v.map(`Dehoisted \`${v[0]}\` to \`${v[1]}\``).join("\n"))
+    )
+    .catch(([errs]) =>
+      ctx.respond(
+        `Couldn't dehoist ${errs.map(([name]) => `\`${name}\``).join(", ")}`
+      )
+    );
+  async function dehoistAll(v) {
+    let successes = [],
+      errs = [];
     v.forEach((v) =>
-    ctx.util.get.member(ctx, v).then((v) => {
-      let name = v.displayName;
-      v.setNickname?.(
-        v.displayName
-          .replace(
-            /(^[!.?,\-\/:;()€&@”[\]{}£\#%\^\*\+=•¥’!\$\?\s]+|(^[\S.]$))/g,
-            ""
+      ctx.util.get.member(ctx, v).then((v) => {
+        let name = v.displayName;
+        v.setNickname?.(
+          v.displayName
+            .replace(
+              /(^[!.?,\-\/:;()€&@”[\]{}£\#%\^\*\+=•¥’!\$\?\s]+|(^[\S.]$))/g,
+              ""
+            )
+            .trim()
+            .replace(/\s+/g, " ")
+        )
+          .then((m) =>
+            // ctx.channel.send(`Dehoisted \`${name}\` to \`${m.displayName}\`.`)
+            successes.push([name, m.displayName])
           )
-          .trim()
-          .replace(/\s+/g, " ")
-      ).then((m) =>
-        // ctx.channel.send(`Dehoisted \`${name}\` to \`${m.displayName}\`.`)
-        successes.push([name, m.displayName])
-      ).catch(e => errs.push([name, e]));
-    })
-  );
-  if (errs.length) throw [errs, successes]
-  return successes
+          .catch((e) => errs.push([name, e]));
+      })
+    );
+    if (errs.length) throw [errs, successes];
+    return successes;
   }
 };
 module.exports.help = {
-  name: '>dehoist',
-  id: 'dehoist',
-  aliases: ['deh'],
-  desc: 'Dehoists a member.',
-  category: 'moderation',
+  name: ">dehoist",
+  id: "dehoist",
+  aliases: ["deh"],
+  desc: "Dehoists a member.",
+  category: "moderation",
   whitelisted: false,
-  usage: '>dehoist <member>',
+  usage: ">dehoist <member>",
   permLvl: 2,
-  requiredPerms: ["MANAGE_NICKNAMES"]
+  requiredPerms: [
+    /*"MANAGE_NICKNAMES"*/
+  ],
 };
