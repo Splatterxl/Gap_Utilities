8617d132a8aa6e48eade03492801c40fff8b741d
nearlySplat <splatterxl@outlook.ie>
2021-02-21 21:29:51 +0000

    Update message.event.js

--- events/message.event.js
+++ events/message.event.js
@@ -230,7 +230,7 @@ module.exports = {
           })
         );
       if ((cmd.help?.whitelisted || cmd.help?.permLvl == 6) && !ctx.isOwner) return ctx.respond(ctx.util.embeds.notWhitelisted());
-      if (cmd.help?.permLvl >= 2 && !(ctx.message.member.permissions.has("KICK_MEMBERS") && !ctx.isOwner) && cmd.help?.permLvl !== 6) return ctx.respond(ctx.util.embeds.errorEmbed("You need to be a Helper+ to use this command!"))
+      if (cmd.help?.permLvl >= 2 && (!ctx.message.member.permissions.has("KICK_MEMBERS") && !ctx.isOwner) && cmd.help?.permLvl !== 6) return ctx.respond(ctx.util.embeds.errorEmbed("You need to be a Helper+ to use this command!"))
       try {
         await cmd?.run?.(bot, msg, ctx.args, db, flags, ctx);
       } catch (e) {
