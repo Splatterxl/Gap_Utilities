d9409ecc4d41d6ba09ae80de45c323b78f928cd9
nearlySplat <splatterxl@outlook.ie>
2021-02-23 17:17:51 +0000

    Update templang.js

--- misc/templang.js
+++ misc/templang.js
@@ -4,11 +4,11 @@ function templang(str, obj = {}) {
   // matched strings, you could use 'matched.map(value => eval(`obj.${value}`))' if you wanted.
   const matched = str
     .match(/{{[^}]+}}/g)
-    .map((v) => v.replace(/::/g, "."))
+    ?.map((v) => v.replace(/::/g, "."))
     .map((v) => v.replace(/(^{{|}}$)/g, ""));
   // values from the matched output and the object
   const values = matched
-    .map((v) => (v.includes(".") ? obj[v.match(/[^\.]+/g)[0]] : v))
+    ?.map((v) => (v.includes(".") ? obj[v.match(/[^\.]+/g)[0]] : v))
     .map((v, i) => {
       let currentEl = v;
       for (let element of matched[i].match(/[^.]+/g).slice(1)) {
@@ -17,7 +17,7 @@ function templang(str, obj = {}) {
       return currentEl;
     });
   // modify the string
-  matched.map((v, index) => {
+  matched?.map((v, index) => {
     while (str.includes(`{{${v.replace(/\./g, "::")}}}`))
       str = str.replace(
         new RegExp(`{{${v.replace(/\./g, "::")}}}`, "g"),
