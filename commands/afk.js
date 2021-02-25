9fd217a1fd4d88430ddac33622ecdbeb19badedb
nearlySplat <splatterxl@outlook.ie>
2021-02-25 17:54:28 +0000

    Create afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -19,22 +19,14 @@ class Afk extends BaseCommand {
       async (bot, msg, args, db, flags, ctx) => {
         try {
           let canSetNickname = true;
-          if (!msg.guild.me.permissions.has('MANAGE_NICKNAMES'))
+          if (!msg.guild.me.permissions.has('MANAGE_NICKNAMES') || !msg.member.manageable)
             canSetNickname = false;
-          if (msg.member.displayName.startsWith('[AFK]') && canSetNickname) {
-            msg.member
-              .setNickname(msg.member.displayName.slice(6))
-              .catch(e => {});
-            // @ts-ignore
-            msg.channel.send(embeds.afkRemove(msg));
-          }
-          if (msg.member.displayName.startsWith('[AFK]')) return;
 
           db.set(
             `afk.${msg.author.id}`,
             args[1] ? args.slice(1).join(' ') : 'No reason specified.'
           );
-
+          if (canSetNickname) msg.member.setNickname(`[AFK] ${msg.member.displayName}`).catch(() => {})
           ctx.respond(
             new Discord.MessageEmbed({
               color: 'GREEN',
