6724b0e7f58e9423d9880072499022c209cf01c1
nearlySplat <splatterxl@outlook.ie>
2021-03-06 19:45:40 +0000

    Update guildMemberAdd.event.js

--- events/guildMemberAdd.event.js
+++ events/guildMemberAdd.event.js
@@ -4,5 +4,5 @@ module.exports.run = async (member, db, client) => {
   if (!db.get(`settings.g${member.guild.id}.joinNotifs`)) return;
   const channelId = db.get(`settings.g${member.guild.id}.joinNotifs`),
     channel = await client.channels.fetch(channelId).catch(e => null);
-  channel.send(`**${member.user.tag}** joined! Welcome!`).catch(e => null)
+  channel.send(db.get(`settings.g${member.guild.id}.joinTemplate`)?.replace(/{{tag}}/g, member.user.tag) ?? `**${member.user.tag}** joined! Welcome!`).catch(e => null)
 }
